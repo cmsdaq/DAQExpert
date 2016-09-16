57d84cfc56adf2d20d140a65aef6de97cdb4c560
gladky <maciej.gladki@gmail.com>
2016-09-16 15:04:21 +0200

    test() linear segmentation tested

---
+++ src/test/java/rcms/utilities/daqexpert/segmentation/LinearSegmentatorTest.java
@@ -0,0 +1,170 @@
+package rcms.utilities.daqexpert.segmentation;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map.Entry;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import rcms.utilities.daqexpert.Point;
+
+public class LinearSegmentatorTest {
+
+	private static Logger logger = Logger.getLogger(LinearSegmentatorTest.class);
+
+	@BeforeClass
+	public static void prepare() throws URISyntaxException, JsonParseException, JsonMappingException, IOException {
+		ObjectMapper mapper = new ObjectMapper();
+
+		java.net.URL url = LinearSegmentator.class.getResource("1h.json");
+		File file = new File(url.toURI());
+
+		logger.info("Deserialize file: " + file.getAbsolutePath());
+
+		Class<List<Pair<Long, Float>>> clazz = (Class) List.class;
+
+		List<Pair<Long, Float>> result = mapper.readValue(file, clazz);
+
+		logger.info("There is " + result.size());
+
+		realDataExample = new ArrayList<>();
+		Iterator<?> iterator = result.iterator();
+		while (iterator.hasNext()) {
+
+			Object entry = iterator.next();
+
+			// logger.info(entry.toString());
+			// logger.info(entry.getClass());
+			LinkedHashMap lentry = (LinkedHashMap) entry;
+
+			Entry llentry = (Entry) lentry.entrySet().iterator().next();
+
+			Long time = Long.valueOf((String) llentry.getKey());
+			float value = ((Double) llentry.getValue()).floatValue();
+
+			logger.debug(llentry.getKey());
+			logger.debug(llentry.getValue());
+			realDataExample.add(new Point(time, value));
+		}
+
+		Assert.assertEquals(1327, realDataExample.size());
+	}
+
+	private static List<Point> realDataExample;
+
+	@Test
+	public void minuteResolutionSegmentationTest() {
+		LinearSegmentator ls = new LinearSegmentator(SegmentationSettings.Minute);
+		List<Point> outputStream = ls.segmentate(realDataExample);
+		printDataToVisualize(realDataExample);
+		printDataToVisualize(outputStream);
+		Assert.assertEquals(441, outputStream.size());
+	}
+
+	@Test
+	public void hourResolutionSegmentationTest() {
+		LinearSegmentator ls = new LinearSegmentator(SegmentationSettings.Hour);
+		List<Point> outputStream = ls.segmentate(realDataExample);
+		printDataToVisualize(realDataExample);
+		printDataToVisualize(outputStream);
+		Assert.assertEquals(93, outputStream.size());
+	}
+
+	@Test
+	public void dayResolutionSegmentationTest() {
+		LinearSegmentator ls = new LinearSegmentator(SegmentationSettings.Day);
+		List<Point> outputStream = ls.segmentate(realDataExample);
+		printDataToVisualize(realDataExample);
+		printDataToVisualize(outputStream);
+		Assert.assertEquals(44, outputStream.size());
+	}
+
+	@Test
+	public void monthResolutionSegmentationTest() {
+		LinearSegmentator ls = new LinearSegmentator(SegmentationSettings.Month);
+		List<Point> outputStream = ls.segmentate(realDataExample);
+		printDataToVisualize(realDataExample);
+		printDataToVisualize(outputStream);
+		Assert.assertEquals(18, outputStream.size());
+	}
+
+	private void printDataToVisualize(List<Point> stream) {
+		ObjectMapper om = new ObjectMapper();
+		try {
+			String result = om.writeValueAsString(stream);
+			System.out.println(result);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void constantValueTest() {
+
+		List<Point> inputStream = new ArrayList<Point>();
+
+		inputStream.add(new Point(1, 0));
+		inputStream.add(new Point(2, 0));
+		inputStream.add(new Point(3, 0));
+
+		LinearSegmentator ls = new LinearSegmentator(SegmentationSettings.Day);
+
+		List<Point> outputStream = ls.segmentate(inputStream);
+
+		logger.debug(inputStream);
+		logger.debug(outputStream);
+	}
+
+	@Test
+	public void lineTest() {
+
+		List<Point> inputStream = new ArrayList<Point>();
+
+		inputStream.add(new Point(1, 3));
+		inputStream.add(new Point(2, 4));
+		inputStream.add(new Point(3, 5));
+
+		LinearSegmentator ls = new LinearSegmentator(SegmentationSettings.Day);
+
+		List<Point> outputStream = ls.segmentate(inputStream);
+
+		logger.debug(inputStream);
+		logger.debug(outputStream);
+	}
+
+	@Test
+	public void twoLinesTest() {
+
+		List<Point> inputStream = new ArrayList<Point>();
+
+		inputStream.add(new Point(1, 3));
+		inputStream.add(new Point(2, 4));
+		inputStream.add(new Point(3, 5));
+		inputStream.add(new Point(4, 3));
+		inputStream.add(new Point(5, 1));
+		inputStream.add(new Point(6, -1));
+
+		LinearSegmentator ls = new LinearSegmentator(SegmentationSettings.Day);
+
+		List<Point> outputStream = ls.segmentate(inputStream);
+
+		logger.debug(inputStream);
+		logger.debug(outputStream);
+	}
+
+}

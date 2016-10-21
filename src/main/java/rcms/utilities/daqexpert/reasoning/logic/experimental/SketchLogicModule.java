e697ccec66ba34f66f398c0ffded8d3a060f2816
gladky <maciej.gladki@gmail.com>
2016-10-21 10:37:48 +0200

    style(): update comments on sketch LM

--- src/main/java/rcms/utilities/daqexpert/reasoning/logic/experimental/SketchLogicModule.java
+++ src/main/java/rcms/utilities/daqexpert/reasoning/logic/experimental/SketchLogicModule.java
@@ -9,36 +9,27 @@ import rcms.utilities.daqexpert.reasoning.base.enums.EventPriority;
 
 /**
  * This is an sketch of Logic Module (LM)
- * 
- * @author Maciej Gladki (maciej.szymon.gladki@cern.ch)
+ * @author full-name (email-address)
  */
 public class SketchLogicModule extends SimpleLogicModule {
 
-	/** TODO: 1. Set up basic information about this Logic Module here */
 	public SketchLogicModule() {
-		this.name = "experimental"; // TODO: 1a. set descriptive name
-		this.description = "Easy start sketch LM"; // TODO 1b. provide more info
+		
+		/* TODO: 1. set following fields: name, description */
+		this.name = "experimental";
+		this.description = "quick start LM";
 		this.group = EventGroup.EXPERIMENTAL;
 		this.priority = EventPriority.EXPERIMENTAL;
 	}
 
 	/**
-	 * TODO: 2. Implement condition of this LM here. Return true when
-	 * your condition is satisfied, otherwise return false.
-	 * 
-	 * @param snapshot
-	 *            access any parameter of DAQ system using this object
-	 * @param results
-	 *            you can reuse other LMs results by accessing this object
-	 *            e.g. results.get(NoRate.class.getSimpleName())
-	 * @return return true when condition satisfied, false otherwise.
-	 * 
+	 * Returns true when the condition is satisfied, 
+	 * otherwise returns false.
 	 */
 	@Override
 	public boolean satisfied(DAQ snapshot, Map<String, Boolean> results) {
 
-		/* TODO: 2a. implement your condition here */
+		/* TODO: 2. implement your condition here */
 		return false;
 	}
-
 }

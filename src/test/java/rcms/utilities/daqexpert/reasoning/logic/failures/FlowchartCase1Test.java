063591aed7787977a1baa469f2d176c16cc6aab7
Andre Holzner <holzner@lpcmssd02.dyndns.cern.ch>
2017-01-06 18:30:03 +0100

    now running over three snapshots (consecutive in time) to have the correct result of the Transition test, made test objects class members (instead of local variables) such that they keep their state across timestamps

--- src/test/java/rcms/utilities/daqexpert/reasoning/logic/failures/FlowchartCase1Test.java
+++ src/test/java/rcms/utilities/daqexpert/reasoning/logic/failures/FlowchartCase1Test.java
@@ -4,7 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -25,8 +27,16 @@ import rcms.utilities.daqexpert.reasoning.logic.basic.Transition;
  */
 public class FlowchartCase1Test
 {
-  private static DAQ snapshot;
+  private static List<DAQ> snapshots = new ArrayList();
   
+	Transition transition     = new Transition();
+	RunOngoing runOngoing     = new RunOngoing();
+  StableBeams stableBeams   = new StableBeams();
+  ExpectedRate expectedRate = new ExpectedRate();
+  NoRate noRate             = new NoRate();
+  NoRateWhenExpected nrwe   = new NoRateWhenExpected();
+  FlowchartCase1 fc1        = new FlowchartCase1();
+	
   public FlowchartCase1Test()
   {
   }
@@ -46,58 +56,65 @@ public class FlowchartCase1Test
   @BeforeClass
   public static void prepare() throws URISyntaxException, IOException {
 
-    StructureSerializer serializer = new StructureSerializer();
+		snapshots.add(getSnapshot("1480809938304.smile"));
+		snapshots.add(getSnapshot("1480809943439.smile"));
+    snapshots.add(getSnapshot("1480809948643.smile"));
+
+	}
+	
+	/** runs tests on a single snapshot */
+	private boolean testSingle(DAQ snapshot)
+	{
+		Map<String, Boolean> results = new HashMap();
     
-    java.net.URL url = FlowchartCase1.class.getResource("1480809927880.smile");
-		File file = new File(url.toURI());
-		
-    snapshot = serializer.deserialize(file.getAbsolutePath(), 
-            PersistenceFormat.SMILE);
+    boolean res = false;
     
-  }
-  
+    // required by ExpectedRate
+    res = runOngoing.satisfied(snapshot, results);
+    results.put(RunOngoing.class.getSimpleName(), res);
+    
+    // required by NoRateWhenExpected
+    res = stableBeams.satisfied(snapshot, results);
+    results.put(StableBeams.class.getSimpleName(), res);
+
+    res = expectedRate.satisfied(snapshot, results);
+    results.put(ExpectedRate.class.getSimpleName(), res);
+    
+    res = noRate.satisfied(snapshot, results);
+    results.put(NoRate.class.getSimpleName(), res);
+
+    res = transition.satisfied(snapshot, results);
+    results.put(Transition.class.getSimpleName(), res);
+    
+    // required by FlowchartCase1
+    res = nrwe.satisfied(snapshot, results);
+    results.put(NoRateWhenExpected.class.getSimpleName(), res);
+    
+    fc1.getContext().clearContext();
+    boolean result = fc1.satisfied(snapshot, results);
+		return result;
+	}
+	
   /**
    * Test of satisfied method, of class FlowchartCase1.
    */
   @Test
   public void testSatisfied()
   {
-    Map<String, Boolean> results = new HashMap();
-    
-    boolean res = false;
-    
-    // required by ExpectedRate
-    RunOngoing runOngoing = new RunOngoing();
-    res = runOngoing.satisfied(snapshot, results);
-    results.put(RunOngoing.class.getSimpleName(), res);
-    
-    // required by NoRateWhenExpected
-    StableBeams stableBeams = new StableBeams();
-    res = stableBeams.satisfied(snapshot, results);
-    results.put(StableBeams.class.getSimpleName(), res);
+    Boolean result = null;
+		
+		for (DAQ snapshot : snapshots)
+		{
+			// note that we run over three snapshots in time such that 
+			// the Transition test returns the expected result. We
+			// only look at the result of the FlowchartCase1 test
+			// of the last snapshot
+			result = testSingle(snapshot);
+		}
 
-    ExpectedRate expectedRate = new ExpectedRate();
-    res = expectedRate.satisfied(snapshot, results);
-    results.put(ExpectedRate.class.getSimpleName(), res);
-    
-    NoRate noRate = new NoRate();
-    res = noRate.satisfied(snapshot, results);
-    results.put(NoRate.class.getSimpleName(), res);
-    
-    Transition transition = new Transition();
-    res = transition.satisfied(snapshot, results);
-    results.put(Transition.class.getSimpleName(), res);
-    
-    // required by FlowchartCase1
-    NoRateWhenExpected nrwe = new NoRateWhenExpected();
-    res = nrwe.satisfied(snapshot, results);
-    results.put(NoRateWhenExpected.class.getSimpleName(), res);
-    
-    FlowchartCase1 instance = new FlowchartCase1();
-    instance.getContext().clearContext();
-    boolean expResult = true;
-    boolean result = instance.satisfied(snapshot, results);
+		boolean expResult = true;
     assertEquals(expResult, result);
-  }
+
+	}
   
 }

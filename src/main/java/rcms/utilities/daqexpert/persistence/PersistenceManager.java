b10a24341b015c8d966ff43b98f0ae621ffdf16e
gladky <maciej.gladki@gmail.com>
2017-01-17 10:07:00 +0100

    feat(persistence): add persistence manager and tests

--- src/main/java/rcms/utilities/daqexpert/persistence/PersistenceManager.java
+++ src/main/java/rcms/utilities/daqexpert/persistence/PersistenceManager.java
@@ -3,14 +3,53 @@ package rcms.utilities.daqexpert.persistence;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+
 public class PersistenceManager {
 
-	public void persist(Entry entry) {
+	private final EntityManagerFactory entityManagerFactory;
+	private final EntityManager entityManager;
 
+	public PersistenceManager(String persistenceUnitName) {
+		entityManagerFactory = Persistence.createEntityManagerFactory(persistenceUnitName);
+		entityManager = entityManagerFactory.createEntityManager();
 	}
 
-	public List<Entry> getEntries(Date start, Date end) {
-		return null;
+	public void persist(Entry entry) {
+		entityManager.getTransaction().begin();
+		entityManager.persist(entry);
+		entityManager.getTransaction().commit();
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Entry> getEntries(Date startDate, Date endDate) {
+
+		// TODO: close session?
+		Session session = entityManager.unwrap(Session.class);
+
+		Criteria elementsCriteria = session.createCriteria(Entry.class);
+		elementsCriteria.addOrder(Order.desc("start"));
+		elementsCriteria.add(Restrictions.between("start", startDate, endDate));
+
+		return elementsCriteria.list();
+	}
+
+	public List<Entry> getEntries2(Date start, Date end) {
+		entityManager.getTransaction().begin();
+		List<Entry> result = entityManager.createQuery("from Entry", Entry.class).getResultList();
+		for (Entry event : result) {
+			System.out.println("Event (" + event.getStart() + ") : " + event.getContent());
+		}
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		return result;
 	}
 
 }
--- src/test/java/rcms/utilities/daqexpert/persistence/PersistenceManagerTest.java
+++ src/test/java/rcms/utilities/daqexpert/persistence/PersistenceManagerTest.java
@@ -3,43 +3,36 @@ package rcms.utilities.daqexpert.persistence;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.xml.bind.DatatypeConverter;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class PersistenceManagerTest {
 
 	@Test
-	public void test() {
+	public void simpleTest() {
 
-		Entry entry = new Entry();
-		entry.setContent("test entry");
-		entry.setStart(new Date());
+		PersistenceManager pm = new PersistenceManager("history-test");
 
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("history");
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
+		Date t1 = DatatypeConverter.parseDateTime("2017-01-17T10:35:00Z").getTime();
+		Date t2 = DatatypeConverter.parseDateTime("2017-01-17T11:35:00Z").getTime();
 
-		entityManager.persist(entry);
+		pm.persist(getTestObject(t1, "test1"));
+		pm.persist(getTestObject(t2, "test2"));
 
-		entityManager.getTransaction().commit();
-		entityManager.close();
+		Date ts = DatatypeConverter.parseDateTime("2017-01-17T10:30:00Z").getTime();
+		Date te = DatatypeConverter.parseDateTime("2017-01-17T11:30:00Z").getTime();
+		List<Entry> result = pm.getEntries(ts, te);
 
+		Assert.assertEquals(1, result.size());
 	}
 
-	@Test
-	public void test2() {
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("history");
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-		List<Entry> result = entityManager.createQuery("from Entry", Entry.class).getResultList();
-		for (Entry event : result) {
-			System.out.println("Event (" + event.getStart() + ") : " + event.getContent());
-		}
-		entityManager.getTransaction().commit();
-		entityManager.close();
+	private Entry getTestObject(Date date, String content) {
+		Entry entry = new Entry();
+		entry.setContent(content);
+		entry.setStart(date);
+		return entry;
 	}
 
 }

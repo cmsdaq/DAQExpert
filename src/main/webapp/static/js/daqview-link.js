c74d6dbb8fb4038858be927819fb48934f613589
gladky <maciej.gladki@gmail.com>
2017-03-28 09:24:57 +0200

    feat(): link to daqview - michail format

--- src/main/webapp/static/js/daqview-link.js
+++ src/main/webapp/static/js/daqview-link.js
@@ -45,9 +45,37 @@ $(document).ready(function() {
 
 		var center = start.add(diff / 2, 'milliseconds');
 		console.log("centerf: " + center.toISOString() + ", duration= " + duration.format());
-		time = center;
+		time = formatLocalDate(new Date(center));
+		console.log("Michail format: " + time);
 	}
 	
+	/** 
+	 * Formatting of the date introduced by Michail Vougioukas
+	 */
+	var formatLocalDate = function(date){
+
+        var localTimestampElements = (new Date(date.getTime()).toString()).split(" ");
+        
+        var mapOfMonths =
+    	{
+        		"Jan" : "01",
+        		"Feb" : "02",
+        		"Mar" : "03",
+        		"Apr" : "04",
+        		"May" : "05",
+        		"Jun" : "06",
+        		"Jul" : "07",
+        		"Aug" : "08",
+        		"Sep" : "09",
+        		"Oct" : "10",
+        		"Nov" : "11",
+        		"Dec" : "12"
+    	};
+
+        var formattedLocalTimestamp = localTimestampElements[3]+" "+mapOfMonths[localTimestampElements[1]]+" "+localTimestampElements[2]+" "+localTimestampElements[4];
+
+        return formattedLocalTimestamp;
+   };
 
 
 });

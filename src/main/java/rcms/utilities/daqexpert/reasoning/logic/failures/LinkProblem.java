0f7ab3e106d89280d3bf3bb94b090dcea2a00e7c
gladky <maciej.gladki@gmail.com>
2017-04-03 10:43:01 +0200

    feat(): add subcases of backpressure analysis, update LM registry

---
+++ src/main/java/rcms/utilities/daqexpert/reasoning/logic/failures/LinkProblem.java
@@ -0,0 +1,42 @@
+package rcms.utilities.daqexpert.reasoning.logic.failures;
+
+import java.util.Map;
+
+import rcms.utilities.daqaggregator.data.DAQ;
+import rcms.utilities.daqexpert.reasoning.logic.basic.NoRateWhenExpected;
+
+/**
+ * @author Maciej Gladki (maciej.szymon.gladki@cern.ch)
+ *
+ */
+public class LinkProblem extends BackpressureAnalyzer {
+
+	public LinkProblem() {
+		this.name = "Link problem";
+
+		this.description = "Link problem detected. "
+				+ "RU {{AFFECTED-RU}} is waiting for backpressured FED {{AFFECTED-FED}}. "
+				+ "FED belongs to TTCP {{AFFECTED-TTCP}} in {{AFFECTED-SUBSYSTEM}} subsystem. "
+				+ "FED is in {{AFFECTED-TTCP-STATE}} and stopped sending data.";
+
+		this.action = null;
+
+	}
+
+	@Override
+	public boolean satisfied(DAQ daq, Map<String, Boolean> results) {
+
+		if (!results.get(NoRateWhenExpected.class.getSimpleName()))
+			return false;
+
+		boolean result = false;
+		assignPriority(results);
+
+		Subcase backpressureRootCase = detectBackpressure(daq);
+		if (backpressureRootCase == Subcase.LinkProblem) {
+			result = true;
+		}
+		return result;
+	}
+
+}

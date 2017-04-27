6f49d73d2e502c6bac2de0dbfe10d70215c6e2ad
Andre Holzner <holzner@lpcmssd02.dyndns.cern.ch>
2017-04-27 10:30:07 +0200

    added a new test case for parsing FED numbers for flow chart case 1, partially addressing issue #57

--- src/test/java/rcms/utilities/daqexpert/reasoning/logic/failures/FlowchartCase1Test.java
+++ src/test/java/rcms/utilities/daqexpert/reasoning/logic/failures/FlowchartCase1Test.java
@@ -3,12 +3,15 @@ package rcms.utilities.daqexpert.reasoning.logic.failures;
 import static org.junit.Assert.assertEquals;
 
 import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 import org.junit.Test;
 
 import rcms.utilities.daqaggregator.data.DAQ;
+import rcms.utilities.daqexpert.reasoning.base.Context;
 
 /**
  *
@@ -42,4 +45,33 @@ public class FlowchartCase1Test extends FlowchartCaseTestBase {
 
 	}
 
+	/** test case with a different error message, originally
+	 *  did not identify FED and TTC partition correctly.
+	 */
+	@Test
+	public void testFEDparsing() throws URISyntaxException {
+		DAQ snapshot = getSnapshot("1493263275021.smile");
+		Map<String, Boolean> results = new HashMap();
+
+		// put results of prerequisite tests by hand
+		// (as opposed to get them from a series of snapshots
+		// which introduces a dependency on other tests)
+
+		results.put("StableBeams", false);
+		results.put("NoRateWhenExpected", true);
+
+		assertEquals(true, fc1.satisfied(snapshot, results));
+		
+		Context context = fc1.getContext();
+		assertEquals(new HashSet(Arrays.asList("622")),  context.getContext().get("FED"));
+		assertEquals(new HashSet(Arrays.asList("ECAL")), context.getContext().get("SUBSYSTEM"));
+		assertEquals(new HashSet(Arrays.asList("EB-")),  context.getContext().get("TTCP"));
+		
+		assertEquals(false, fc2.satisfied(snapshot, results));
+		assertEquals(false, fc3.satisfied(snapshot, results));
+		assertEquals(false, fc4.satisfied(snapshot, results));
+		assertEquals(false, fc5.satisfied(snapshot, results));
+		assertEquals(false, fc6.satisfied(snapshot, results));
+
+	}
 }

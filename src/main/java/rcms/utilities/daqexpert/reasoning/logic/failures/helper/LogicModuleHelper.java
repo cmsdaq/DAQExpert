b3b37cfa2f6c37a4994ed03606f30b38d54c3f64
Andre Holzner <holzner@lpcmssd02.dyndns.cern.ch>
2017-06-13 17:22:14 +0200

    added a test class (and test snapshot) for new class LogicModuleHelper

--- src/main/java/rcms/utilities/daqexpert/reasoning/logic/failures/helper/LogicModuleHelper.java
+++ src/main/java/rcms/utilities/daqexpert/reasoning/logic/failures/helper/LogicModuleHelper.java
@@ -1,9 +1,12 @@
 package rcms.utilities.daqexpert.reasoning.logic.failures.helper;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import rcms.utilities.daqaggregator.data.DAQ;
 import rcms.utilities.daqaggregator.data.FED;
 import rcms.utilities.daqaggregator.data.RU;
+import rcms.utilities.daqaggregator.data.SubSystem;
 
 /**
  *  Some utility functions used by the logic modules.
@@ -37,6 +40,45 @@ public class LogicModuleHelper
 		FED refFED = feds.iterator().next();
 
 		return refFED.getEventCounter();
+	}
+
+	/** @return a list of FEDs which have sent fewer events than the TCDS.
+	 *   Ignores those which are FRL masked or do not have an FRL (pseudofeds).
+	 *
+	 *   This can be used to find the FEDs which block data taking because
+	 *   they are not sending fragments anymore (if the trigger rate is zero).
+	 */
+	public static List<FED> getFedsWithFewerFragments(DAQ daq) {
+
+		List<FED> result = new ArrayList<>();
+
+		Long refCounter = getRefEventCounter(daq);
+
+		if (refCounter == null) {
+			// we can't find the EVM, there is a problem
+			return result;
+		}
+
+		for (FED fed : daq.getFeds()) {
+
+			if (! fed.isHasSLINK()) {
+				// pseudofed, can't block data taking by not sending triggers
+				continue;
+			}
+
+			if (fed.isFrlMasked()) {
+				// FED's slink is masked
+				continue;
+			}
+
+			if (fed.getEventCounter() < refCounter) {
+				// this FED is behind
+				result.add(fed);
+			}
+
+		} // loop over FEDs
+
+		return result;
 	}
 
 }

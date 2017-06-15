335d3065cea1c46e17a3c8a8c1b2c3da9f389fb4
gladky <maciej.gladki@gmail.com>
2017-06-15 16:14:17 +0200

    feat(): add problematic subsystems to context

--- src/main/java/rcms/utilities/daqexpert/reasoning/logic/failures/StuckAfterSoftError.java
+++ src/main/java/rcms/utilities/daqexpert/reasoning/logic/failures/StuckAfterSoftError.java
@@ -1,23 +1,50 @@
 package rcms.utilities.daqexpert.reasoning.logic.failures;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
 
 import rcms.utilities.daqaggregator.data.DAQ;
+import rcms.utilities.daqaggregator.data.SubSystem;
 
 public class StuckAfterSoftError extends KnownFailure {
 
-	private String precedingState = "";
+	/** Previous L0 state */
 	private String previousState = "";
+
+	/** Preceding L0 state - always different than current state */
+	private String precedingState = "";
+
+	/**
+	 * Required preceding state of L0 to toggle this logic module. It is
+	 * followed by <code>stateToToggle</code>
+	 * 
+	 * @see StuckAfterSoftError#stateToToggle
+	 */
 	private final String precedingStateToToggle = "FixingSoftError";
+
+	/**
+	 * Required state of L0 to toggle this logic module. It follows the
+	 * <code>precedingStateToToggle</code>
+	 * 
+	 * @see StuckAfterSoftError#precedingStateToToggle
+	 */
 	private final String stateToToggle = "Error";
+
+	/** List of problematic states that will be used in report */
+	private List<String> problemStates = Arrays.asList("FixingSoftError", "RunningSoftErrorDetected");
+
+	/** List of subsystems in problematic states that will be used in report */
+	private List<String> subsystemsInProblematicState = new ArrayList<>();
 
 	private final static Logger logger = Logger.getLogger(StuckAfterSoftError.class);
 
 	public StuckAfterSoftError() {
 		this.name = "Stuck after soft error";
-		this.description = "Stuck after soft error";
+		this.description = "Level zero is stuck after fixing soft error. This is caused by subsystem(s) {{SUBSYSTEM}}";
 	}
 
 	@Override
@@ -32,11 +59,30 @@ public class StuckAfterSoftError extends KnownFailure {
 
 			logger.debug("Condition satisfied: " + precedingState + ", current state: " + currentState);
 			result = true;
+
+			for (String subsystemInProblematicState : subsystemsInProblematicState) {
+				context.register("SUBSYSTEM", subsystemInProblematicState);
+			}
+
 		}
 
 		if (!previousState.equalsIgnoreCase(currentState)) {
-			logger.debug("Changing state from: " + precedingState + " to " + currentState);
+			logger.debug("Changing state from: " + previousState + " to " + currentState);
 			precedingState = previousState;
+
+			if (!stateToToggle.equalsIgnoreCase(currentState)) {
+				logger.debug("Clearing past information about subsystems in problematic state: "
+						+ subsystemsInProblematicState.size());
+				subsystemsInProblematicState.clear();
+			}
+		}
+
+		if (precedingStateToToggle.equalsIgnoreCase(currentState)) {
+			for (SubSystem subsystem : daq.getSubSystems()) {
+				if (problemStates.contains(subsystem.getStatus())) {
+					subsystemsInProblematicState.add(subsystem.getName());
+				}
+			}
 		}
 
 		previousState = currentState;

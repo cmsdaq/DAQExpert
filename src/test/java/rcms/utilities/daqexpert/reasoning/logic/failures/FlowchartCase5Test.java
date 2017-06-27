995c0c8482ed94e7b0a1fc92214d27f9c1ea4392
gladky <maciej.gladki@gmail.com>
2017-06-27 11:32:32 +0200

    test(): add test cases to fed-stuck LM (FC5)

--- src/test/java/rcms/utilities/daqexpert/reasoning/logic/failures/FlowchartCase5Test.java
+++ src/test/java/rcms/utilities/daqexpert/reasoning/logic/failures/FlowchartCase5Test.java
@@ -1,10 +1,15 @@
 package rcms.utilities.daqexpert.reasoning.logic.failures;
 
+import static org.junit.Assert.assertEquals;
+
 import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.HashSet;
 
 import org.junit.Test;
 
 import rcms.utilities.daqaggregator.data.DAQ;
+import rcms.utilities.daqexpert.reasoning.base.Context;
 
 /**
  *
@@ -15,7 +20,67 @@ public class FlowchartCase5Test extends FlowchartCaseTestBase {
 	@Test
 	public void case1Test() throws URISyntaxException {
 		DAQ snapshot = getSnapshot("1479614378467.smile");
+		assertLMOutputFalse(snapshot);
+		Context context = fc5.getContext();
+		assertEquals(new HashSet(Arrays.asList("TRACKER")), context.getContext().get("SUBSYSTEM"));
+		assertEquals(new HashSet(Arrays.asList("TEC-")), context.getContext().get("TTCP"));
+		assertEquals(new HashSet(Arrays.asList(169)), context.getContext().get("FED"));
+	}
 
+	@Test
+	public void case2Test() throws URISyntaxException {
+		DAQ snapshot = getSnapshot("1498440505470.smile");
+		assertLMOutputFalse(snapshot);
+		Context context = fc5.getContext();
+		assertEquals(new HashSet(Arrays.asList("TRACKER")), context.getContext().get("SUBSYSTEM"));
+		assertEquals(new HashSet(Arrays.asList("TIBTID")), context.getContext().get("TTCP"));
+		assertEquals(new HashSet(Arrays.asList(149)), context.getContext().get("FED"));
+	}
+
+	@Test
+	public void case3Test() throws URISyntaxException {
+		DAQ snapshot = getSnapshot("1498096885568.smile");
+
+		assertEqualsAndUpdateResults(false, fc1, snapshot);
+		assertEqualsAndUpdateResults(false, fc2, snapshot);
+		assertEqualsAndUpdateResults(false, ruFailed, snapshot);
+		assertEqualsAndUpdateResults(false, fc3, snapshot);
+
+		// new subcases of old flowchart case 4
+		assertEqualsAndUpdateResults(false, piDisconnected, snapshot);
+		assertEqualsAndUpdateResults(false, piProblem, snapshot);
+		assertEqualsAndUpdateResults(false, fedDisconnected, snapshot);
+		assertEqualsAndUpdateResults(false, fmmProblem, snapshot);
+
+		assertEqualsAndUpdateResults(true, fc5, snapshot);
+		assertEqualsAndUpdateResults(false, fc6, snapshot);
+
+		// TODO: why ferol fifo stuck here?
+		assertEqualsAndUpdateResults(true, ferolFifoStuck, snapshot);
+
+		assertEqualsAndUpdateResults(false, unidentified, snapshot);
+		
+		
+		Context context = fc5.getContext();
+		assertEquals(new HashSet(Arrays.asList("CSC")), context.getContext().get("SUBSYSTEM"));
+		assertEquals(new HashSet(Arrays.asList("CSC+")), context.getContext().get("TTCP"));
+		assertEquals(new HashSet(Arrays.asList(838)), context.getContext().get("FED"));
+	}
+	
+	@Test
+	public void case4Test() throws URISyntaxException {
+		DAQ snapshot = getSnapshot("1497513136376.smile");
+		assertLMOutputFalse(snapshot);
+		Context context = fc5.getContext();
+		assertEquals(new HashSet(Arrays.asList("TRACKER")), context.getContext().get("SUBSYSTEM"));
+		assertEquals(new HashSet(Arrays.asList("TIBTID")), context.getContext().get("TTCP"));
+		assertEquals(new HashSet(Arrays.asList(83)), context.getContext().get("FED"));
+	}
+
+	
+	
+
+	private void assertLMOutputFalse(DAQ snapshot) {
 		assertEqualsAndUpdateResults(false, fc1, snapshot);
 		assertEqualsAndUpdateResults(false, fc2, snapshot);
 		assertEqualsAndUpdateResults(false, ruFailed, snapshot);
@@ -33,7 +98,6 @@ public class FlowchartCase5Test extends FlowchartCaseTestBase {
 		assertEqualsAndUpdateResults(false, ferolFifoStuck, snapshot);
 
 		assertEqualsAndUpdateResults(false, unidentified, snapshot);
-
 	}
 
 }

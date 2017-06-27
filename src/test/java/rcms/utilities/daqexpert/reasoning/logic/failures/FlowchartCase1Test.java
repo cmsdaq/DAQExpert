372cd6910271950292923e72301679543c6f3a80
gladky <maciej.gladki@gmail.com>
2017-06-27 10:58:37 +0200

    test(): new test cases for out-of-sequence data received (FC1)

--- src/test/java/rcms/utilities/daqexpert/reasoning/logic/failures/FlowchartCase1Test.java
+++ src/test/java/rcms/utilities/daqexpert/reasoning/logic/failures/FlowchartCase1Test.java
@@ -27,22 +27,7 @@ public class FlowchartCase1Test extends FlowchartCaseTestBase {
 		DAQ snapshot = getSnapshot("1480809948643.smile");
 
 		assertEqualsAndUpdateResults(true, fc1, snapshot);
-		assertEqualsAndUpdateResults(false, fc2, snapshot);
-		assertEqualsAndUpdateResults(false, ruFailed, snapshot);
-		assertEqualsAndUpdateResults(false, fc3, snapshot);
-
-		// new subcases of old flowchart case 4
-		assertEqualsAndUpdateResults(false, piDisconnected, snapshot);
-		assertEqualsAndUpdateResults(false, piProblem, snapshot);
-		assertEqualsAndUpdateResults(false, fedDisconnected, snapshot);
-		assertEqualsAndUpdateResults(false, fmmProblem, snapshot);
-
-		assertEqualsAndUpdateResults(false, fc5, snapshot);
-		assertEqualsAndUpdateResults(false, fc6, snapshot);
-
-		assertEqualsAndUpdateResults(false, ferolFifoStuck, snapshot);
-
-		assertEqualsAndUpdateResults(false, unidentified, snapshot);
+		assertRestFalse(snapshot);
 
 	}
 
@@ -54,7 +39,8 @@ public class FlowchartCase1Test extends FlowchartCaseTestBase {
 		DAQ snapshot = getSnapshot("1493263275021.smile");
 
 		assertEqualsAndUpdateResults(true, fc1, snapshot);
-		
+		assertRestFalse(snapshot);
+
 		System.out.println(fc1.getDescriptionWithContext());
 		System.out.println(fc1.getActionWithContext());
 
@@ -75,6 +61,7 @@ public class FlowchartCase1Test extends FlowchartCaseTestBase {
 		DAQ snapshot = getSnapshot("1480809948643.smile");
 
 		assertEqualsAndUpdateResults(true, fc1, snapshot);
+		assertRestFalse(snapshot);
 
 		System.out.println(fc1.getDescriptionWithContext());
 		System.out.println(fc1.getActionWithContext());
@@ -84,5 +71,70 @@ public class FlowchartCase1Test extends FlowchartCaseTestBase {
 		assertEquals(new HashSet(Arrays.asList("ES")), context.getContext().get("SUBSYSTEM"));
 		assertEquals(new HashSet(Arrays.asList("ES+")), context.getContext().get("TTCP"));
 
+	}
+
+	@Test
+	public void case03Test() throws URISyntaxException {
+		DAQ snapshot = getSnapshot("1497898122474.smile");
+
+		assertEqualsAndUpdateResults(true, fc1, snapshot);
+		assertRestFalse(snapshot);
+
+		System.out.println(fc1.getDescriptionWithContext());
+		System.out.println(fc1.getActionWithContext());
+
+		Context context = fc1.getContext();
+		assertEquals(new HashSet(Arrays.asList("582")), context.getContext().get("FED"));
+		assertEquals(new HashSet(Arrays.asList("CTPPS_TOT")), context.getContext().get("SUBSYSTEM"));
+		assertEquals(new HashSet(Arrays.asList("TOTDET")), context.getContext().get("TTCP"));
+
+	}
+
+	@Test
+	public void case04Test() throws URISyntaxException {
+		DAQ snapshot = getSnapshot("1496315027862.smile");
+
+		assertEqualsAndUpdateResults(true, fc1, snapshot);
+		assertRestFalse(snapshot);
+
+		System.out.println(fc1.getDescriptionWithContext());
+		System.out.println(fc1.getActionWithContext());
+
+		Context context = fc1.getContext();
+		assertEquals(new HashSet(Arrays.asList("1326")), context.getContext().get("FED"));
+		assertEquals(new HashSet(Arrays.asList("PIXEL")), context.getContext().get("SUBSYSTEM"));
+		assertEquals(new HashSet(Arrays.asList("FPIXM")), context.getContext().get("TTCP"));
+
+	}
+
+	@Test
+	public void case05Test() throws URISyntaxException {
+		DAQ snapshot = getSnapshot("1495916283277.smile");
+
+		assertEqualsAndUpdateResults(true, fc1, snapshot);
+		assertRestFalse(snapshot);
+
+		System.out.println(fc1.getDescriptionWithContext());
+		System.out.println(fc1.getActionWithContext());
+
+		Context context = fc1.getContext();
+		assertEquals(new HashSet(Arrays.asList("1241")), context.getContext().get("FED"));
+		assertEquals(new HashSet(Arrays.asList("PIXEL")), context.getContext().get("SUBSYSTEM"));
+		assertEquals(new HashSet(Arrays.asList("BPIXP")), context.getContext().get("TTCP"));
+
+	}
+
+	private void assertRestFalse(DAQ snapshot) {
+		assertEqualsAndUpdateResults(false, fc2, snapshot);
+		assertEqualsAndUpdateResults(false, ruFailed, snapshot);
+		assertEqualsAndUpdateResults(false, fc3, snapshot);
+		assertEqualsAndUpdateResults(false, piDisconnected, snapshot);
+		assertEqualsAndUpdateResults(false, piProblem, snapshot);
+		assertEqualsAndUpdateResults(false, fedDisconnected, snapshot);
+		assertEqualsAndUpdateResults(false, fmmProblem, snapshot);
+		assertEqualsAndUpdateResults(false, fc5, snapshot);
+		assertEqualsAndUpdateResults(false, fc6, snapshot);
+		assertEqualsAndUpdateResults(false, ferolFifoStuck, snapshot);
+		assertEqualsAndUpdateResults(false, unidentified, snapshot);
 	}
 }

5af1ab3694e238d6f4e15f10fa66debad5fb4b17
gladky <maciej.gladki@gmail.com>
2017-06-27 11:21:49 +0200

    test(): add test cases to partition problem FC3

--- src/test/java/rcms/utilities/daqexpert/reasoning/logic/failures/FlowchartCase3Test.java
+++ src/test/java/rcms/utilities/daqexpert/reasoning/logic/failures/FlowchartCase3Test.java
@@ -1,12 +1,15 @@
 package rcms.utilities.daqexpert.reasoning.logic.failures;
 
+import static org.junit.Assert.assertEquals;
+
 import java.net.URISyntaxException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.HashSet;
 
 import org.junit.Test;
 
 import rcms.utilities.daqaggregator.data.DAQ;
+import rcms.utilities.daqexpert.reasoning.base.Context;
 
 /**
  *
@@ -17,35 +20,84 @@ public class FlowchartCase3Test extends FlowchartCaseTestBase {
 	@Test
 	public void case1Test() throws URISyntaxException {
 		test("1478748186297.smile");
+
+		Context context = fc3.getContext();
+		assertEquals(new HashSet(Arrays.asList("ECAL")), context.getContext().get("SUBSYSTEM"));
+		assertEquals(new HashSet(Arrays.asList("EB-")), context.getContext().get("TTCP"));
 	}
 
 	@Test
 	public void case2Test() throws URISyntaxException {
 		test("1480813540739.smile");
+		Context context = fc3.getContext();
+		assertEquals(new HashSet(Arrays.asList("TRACKER")), context.getContext().get("SUBSYSTEM"));
+		assertEquals(new HashSet(Arrays.asList("TIBTID")), context.getContext().get("TTCP"));
 	}
-	
+
 	@Test
 	public void ttsAtTopFMMNullButPmOutOfSyncTest() throws URISyntaxException {
 		test("1491576714151.smile");
+		Context context = fc3.getContext();
+		assertEquals(new HashSet(Arrays.asList("TRG")), context.getContext().get("SUBSYSTEM"));
+		assertEquals(new HashSet(Arrays.asList("MUTFUP")), context.getContext().get("TTCP"));
 	}
-	
-	
+
+	@Test
+	public void case3Test() throws URISyntaxException {
+		DAQ snapshot = getSnapshot("1497963405145.smile");
+
+		// TODO: why FC1?
+		assertEqualsAndUpdateResults(true, fc1, snapshot);
+		System.out.println(fc1.getDescriptionWithContext());
+
+		assertEqualsAndUpdateResults(false, fc2, snapshot);
+		assertEqualsAndUpdateResults(false, ruFailed, snapshot);
+		assertEqualsAndUpdateResults(true, fc3, snapshot);
+
+		System.out.println(fc3.getDescriptionWithContext());
+
+		// new subcases of old flowchart case 4
+		assertEqualsAndUpdateResults(false, piDisconnected, snapshot);
+		assertEqualsAndUpdateResults(false, piProblem, snapshot);
+		assertEqualsAndUpdateResults(false, fedDisconnected, snapshot);
+		assertEqualsAndUpdateResults(false, fmmProblem, snapshot);
+
+		assertEqualsAndUpdateResults(false, fc5, snapshot);
+		assertEqualsAndUpdateResults(false, fc6, snapshot);
+
+		assertEqualsAndUpdateResults(false, ferolFifoStuck, snapshot);
+
+		assertEqualsAndUpdateResults(false, unidentified, snapshot);
+
+		Context context = fc3.getContext();
+		assertEquals(new HashSet(Arrays.asList("CTPPS_TOT")), context.getContext().get("SUBSYSTEM"));
+		assertEquals(new HashSet(Arrays.asList("TOTDET")), context.getContext().get("TTCP"));
+
+	}
+
+	@Test
+	public void case4Test() throws URISyntaxException {
+		test("1496647349638.smile");
+		Context context = fc3.getContext();
+		assertEquals(new HashSet(Arrays.asList("PIXEL")), context.getContext().get("SUBSYSTEM"));
+		assertEquals(new HashSet(Arrays.asList("BPIXP")), context.getContext().get("TTCP"));
+	}
 
 	private void test(String snapshotFile) throws URISyntaxException {
 
 		DAQ snapshot = getSnapshot(snapshotFile);
-		assertEqualsAndUpdateResults(false, fc1,snapshot );
-		assertEqualsAndUpdateResults(false, fc2,snapshot );
+		assertEqualsAndUpdateResults(false, fc1, snapshot);
+		assertEqualsAndUpdateResults(false, fc2, snapshot);
 		assertEqualsAndUpdateResults(false, ruFailed, snapshot);
-		assertEqualsAndUpdateResults(true, fc3,snapshot );
+		assertEqualsAndUpdateResults(true, fc3, snapshot);
 
 		// new subcases of old flowchart case 4
-		assertEqualsAndUpdateResults(false, piDisconnected,snapshot );
-		assertEqualsAndUpdateResults(false, piProblem,snapshot );
-		assertEqualsAndUpdateResults(false, fedDisconnected,snapshot );
-		assertEqualsAndUpdateResults(false, fmmProblem,snapshot );
+		assertEqualsAndUpdateResults(false, piDisconnected, snapshot);
+		assertEqualsAndUpdateResults(false, piProblem, snapshot);
+		assertEqualsAndUpdateResults(false, fedDisconnected, snapshot);
+		assertEqualsAndUpdateResults(false, fmmProblem, snapshot);
 
-		assertEqualsAndUpdateResults(false, fc5,snapshot );
+		assertEqualsAndUpdateResults(false, fc5, snapshot);
 		assertEqualsAndUpdateResults(false, fc6, snapshot);
 
 		assertEqualsAndUpdateResults(false, ferolFifoStuck, snapshot);

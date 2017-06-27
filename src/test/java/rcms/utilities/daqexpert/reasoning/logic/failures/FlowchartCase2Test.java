a61267c8bd5135f0cfd224142f236ebf83962171
gladky <maciej.gladki@gmail.com>
2017-06-27 11:10:31 +0200

    test(): add more test cases for corrupted data received (FC2)

--- src/test/java/rcms/utilities/daqexpert/reasoning/logic/failures/FlowchartCase2Test.java
+++ src/test/java/rcms/utilities/daqexpert/reasoning/logic/failures/FlowchartCase2Test.java
@@ -3,10 +3,13 @@ package rcms.utilities.daqexpert.reasoning.logic.failures;
 import static org.junit.Assert.assertEquals;
 
 import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.HashSet;
 
 import org.junit.Test;
 
 import rcms.utilities.daqaggregator.data.DAQ;
+import rcms.utilities.daqexpert.reasoning.base.Context;
 
 /**
  *
@@ -18,26 +21,82 @@ public class FlowchartCase2Test extends FlowchartCaseTestBase {
 	public void case1Test() throws URISyntaxException {
 		DAQ snapshot = getSnapshot("1478793337902.smile");
 
-		assertEqualsAndUpdateResults(false, fc1,snapshot);
-		assertEqualsAndUpdateResults(true, fc2,snapshot);
+		assertEqualsAndUpdateResults(true, fc2, snapshot);
+
+		// TODO: why ruFailed?
 		assertEqualsAndUpdateResults(true, ruFailed, snapshot);
-		assertEqualsAndUpdateResults(false, fc3,snapshot);
-		
+
+		assertEqualsAndUpdateResults(false, fc1, snapshot);
+		assertEqualsAndUpdateResults(false, fc3, snapshot);
 		// new subcases of old flowchart case 4
-		assertEqualsAndUpdateResults(false, piDisconnected,snapshot);
-		assertEqualsAndUpdateResults(false, piProblem,snapshot);
-		assertEqualsAndUpdateResults(false, fedDisconnected,snapshot);
-		assertEqualsAndUpdateResults(false, fmmProblem,snapshot);
-		
-		
-		assertEqualsAndUpdateResults(false, fc5,snapshot);
-		assertEqualsAndUpdateResults(false, fc6,snapshot);
-
+		assertEqualsAndUpdateResults(false, piDisconnected, snapshot);
+		assertEqualsAndUpdateResults(false, piProblem, snapshot);
+		assertEqualsAndUpdateResults(false, fedDisconnected, snapshot);
+		assertEqualsAndUpdateResults(false, fmmProblem, snapshot);
+		assertEqualsAndUpdateResults(false, fc5, snapshot);
+		assertEqualsAndUpdateResults(false, fc6, snapshot);
 		assertEqualsAndUpdateResults(false, ferolFifoStuck, snapshot);
-
 		assertEquals(false, unidentified.satisfied(snapshot, results));
 
+		Context context = fc2.getContext();
+		assertEquals(new HashSet(Arrays.asList(644)), context.getContext().get("FED"));
+		assertEquals(new HashSet(Arrays.asList("ECAL")), context.getContext().get("SUBSYSTEM"));
+		assertEquals(new HashSet(Arrays.asList("EB+")), context.getContext().get("TTCP"));
+	}
 
+	@Test
+	public void case2Test() throws URISyntaxException {
+		DAQ snapshot = getSnapshot("1497448564059.smile");
+
+		assertEqualsAndUpdateResults(true, fc2, snapshot);
+		assertEqualsAndUpdateResults(true, ruFailed, snapshot);
+
+		// TODO: why FC3 and FC6?
+		assertEqualsAndUpdateResults(true, fc3, snapshot);
+		assertEqualsAndUpdateResults(true, fc6, snapshot);
+
+		assertEqualsAndUpdateResults(false, fc1, snapshot);
+		// new subcases of old flowchart case 4
+		assertEqualsAndUpdateResults(false, piDisconnected, snapshot);
+		assertEqualsAndUpdateResults(false, piProblem, snapshot);
+		assertEqualsAndUpdateResults(false, fedDisconnected, snapshot);
+		assertEqualsAndUpdateResults(false, fmmProblem, snapshot);
+		assertEqualsAndUpdateResults(false, fc5, snapshot);
+		assertEqualsAndUpdateResults(false, ferolFifoStuck, snapshot);
+		assertEquals(false, unidentified.satisfied(snapshot, results));
+
+		Context context = fc2.getContext();
+		assertEquals(new HashSet(Arrays.asList(841, 843)), context.getContext().get("FED"));
+		assertEquals(new HashSet(Arrays.asList("CSC")), context.getContext().get("SUBSYSTEM"));
+		// assertEquals(new HashSet(Arrays.asList("CSC+")),
+		// context.getContext().get("TTCP"));
+	}
+
+	@Test
+	public void case3Test() throws URISyntaxException {
+		DAQ snapshot = getSnapshot("1498551636794.smile");
+
+		assertEqualsAndUpdateResults(true, fc2, snapshot);
+
+		// TODO: why ruFailed?
+		assertEqualsAndUpdateResults(true, ruFailed, snapshot);
+
+		assertEqualsAndUpdateResults(false, fc1, snapshot);
+		assertEqualsAndUpdateResults(false, fc3, snapshot);
+		// new subcases of old flowchart case 4
+		assertEqualsAndUpdateResults(false, piDisconnected, snapshot);
+		assertEqualsAndUpdateResults(false, piProblem, snapshot);
+		assertEqualsAndUpdateResults(false, fedDisconnected, snapshot);
+		assertEqualsAndUpdateResults(false, fmmProblem, snapshot);
+		assertEqualsAndUpdateResults(false, fc5, snapshot);
+		assertEqualsAndUpdateResults(false, fc6, snapshot);
+		assertEqualsAndUpdateResults(false, ferolFifoStuck, snapshot);
+		assertEquals(false, unidentified.satisfied(snapshot, results));
+
+		Context context = fc2.getContext();
+		assertEquals(new HashSet(Arrays.asList(622)), context.getContext().get("FED"));
+		assertEquals(new HashSet(Arrays.asList("ECAL")), context.getContext().get("SUBSYSTEM"));
+		assertEquals(new HashSet(Arrays.asList("EB-")), context.getContext().get("TTCP"));
 	}
 
 }

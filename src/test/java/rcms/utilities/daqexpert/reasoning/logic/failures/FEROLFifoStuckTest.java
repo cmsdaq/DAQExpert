15687374307181a952d994d27ee0bd2be50f3f19
Andre Holzner <holzner@lpcmssd02.dyndns.cern.ch>
2017-08-29 16:11:25 +0200

    removed unused import from FEROLFifoStuckTest.java

--- src/test/java/rcms/utilities/daqexpert/reasoning/logic/failures/FEROLFifoStuckTest.java
+++ src/test/java/rcms/utilities/daqexpert/reasoning/logic/failures/FEROLFifoStuckTest.java
@@ -2,7 +2,6 @@ package rcms.utilities.daqexpert.reasoning.logic.failures;
 
 import java.net.URISyntaxException;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import rcms.utilities.daqaggregator.data.DAQ;

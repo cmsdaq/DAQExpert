43e5482cd8ce1fa445a5e07311304a8318ea25e2
mgladki <maciej.gladki@gmail.com>
2017-10-03 10:26:03 +0200

    feat(): instructions for stuck after soft error

--- src/main/java/rcms/utilities/daqexpert/reasoning/logic/failures/fixingSoftErrors/StuckAfterSoftError.java
+++ src/main/java/rcms/utilities/daqexpert/reasoning/logic/failures/fixingSoftErrors/StuckAfterSoftError.java
@@ -9,70 +9,75 @@ import org.apache.log4j.Logger;
 
 import rcms.utilities.daqaggregator.data.DAQ;
 import rcms.utilities.daqaggregator.data.SubSystem;
+import rcms.utilities.daqexpert.reasoning.base.action.SimpleAction;
 import rcms.utilities.daqexpert.reasoning.logic.failures.KnownFailure;
 
 public class StuckAfterSoftError extends KnownFailure {
 
-	/** Previous L0 state */
-	private String previousState = "";
+    public StuckAfterSoftError() {
+        this.name = "Stuck after fixing-soft-error";
+        this.description = "Level zero is stuck after fixing soft error. This is caused by subsystem(s) {{SUBSYSTEM}}";
+        this.action = new SimpleAction("Call the DOC of subsystem {{SUBSYSTEM}}", "Ask the DCS shifter to check the status of subsystem {{SUBSYSTEM}}");
+    }
 
-	/** Preceding L0 state - always different than current state */
-	private String precedingState = "";
+    private final static Logger logger = Logger.getLogger(StuckAfterSoftError.class);
+    /**
+     * Required preceding state of L0 to toggle this logic module. It is
+     * followed by <code>stateToToggle</code>
+     *
+     * @see StuckAfterSoftError#stateToToggle
+     */
+    private final String precedingStateToToggle = "FixingSoftError";
+    /**
+     * Required state of L0 to toggle this logic module. It follows the
+     * <code>precedingStateToToggle</code>
+     *
+     * @see StuckAfterSoftError#precedingStateToToggle
+     */
+    private final String stateToToggle = "Error";
+    /**
+     * Previous L0 state
+     */
+    private String previousState = "";
+    /**
+     * Preceding L0 state - always different than current state
+     */
+    private String precedingState = "";
+    /**
+     * List of problematic states that will be used in report
+     */
+    private List<String> problemStates = Arrays.asList("Error");
 
-	/**
-	 * Required preceding state of L0 to toggle this logic module. It is
-	 * followed by <code>stateToToggle</code>
-	 * 
-	 * @see StuckAfterSoftError#stateToToggle
-	 */
-	private final String precedingStateToToggle = "FixingSoftError";
 
-	/**
-	 * Required state of L0 to toggle this logic module. It follows the
-	 * <code>precedingStateToToggle</code>
-	 * 
-	 * @see StuckAfterSoftError#precedingStateToToggle
-	 */
-	private final String stateToToggle = "Error";
 
-	/** List of problematic states that will be used in report */
-	private List<String> problemStates = Arrays.asList("Error");
+    @Override
+    public boolean satisfied(DAQ daq, Map<String, Boolean> results) {
 
-	private final static Logger logger = Logger.getLogger(StuckAfterSoftError.class);
+        boolean result = false;
+        String currentState = daq.getLevelZeroState();
 
-	public StuckAfterSoftError() {
-		this.name = "Stuck after fixing-soft-error";
-		this.description = "Level zero is stuck after fixing soft error. This is caused by subsystem(s) {{SUBSYSTEM}}";
-	}
+        logger.trace("Checking condition: " + precedingState + ", current state: " + currentState);
 
-	@Override
-	public boolean satisfied(DAQ daq, Map<String, Boolean> results) {
+        if (precedingStateToToggle.equalsIgnoreCase(precedingState) && stateToToggle.equalsIgnoreCase(currentState)) {
 
-		boolean result = false;
-		String currentState = daq.getLevelZeroState();
+            logger.debug("Condition satisfied: " + precedingState + ", current state: " + currentState);
+            result = true;
 
-		logger.trace("Checking condition: " + precedingState + ", current state: " + currentState);
+            for (SubSystem subsystem : daq.getSubSystems()) {
+                if (problemStates.contains(subsystem.getStatus())) {
+                    context.register("SUBSYSTEM", subsystem.getName());
+                }
+            }
 
-		if (precedingStateToToggle.equalsIgnoreCase(precedingState) && stateToToggle.equalsIgnoreCase(currentState)) {
+        }
 
-			logger.debug("Condition satisfied: " + precedingState + ", current state: " + currentState);
-			result = true;
+        if (!previousState.equalsIgnoreCase(currentState)) {
+            logger.debug("Changing state from: " + previousState + " to " + currentState);
+            precedingState = previousState;
+        }
 
-			for (SubSystem subsystem : daq.getSubSystems()) {
-				if (problemStates.contains(subsystem.getStatus())) {
-					context.register("SUBSYSTEM", subsystem.getName());
-				}
-			}
-
-		}
-
-		if (!previousState.equalsIgnoreCase(currentState)) {
-			logger.debug("Changing state from: " + previousState + " to " + currentState);
-			precedingState = previousState;
-		}
-
-		previousState = currentState;
-		return result;
-	}
+        previousState = currentState;
+        return result;
+    }
 
 }

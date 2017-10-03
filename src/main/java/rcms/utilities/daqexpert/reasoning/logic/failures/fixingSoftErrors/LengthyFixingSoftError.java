88a2d780383fee168e7e13fb85df165a0c99ebe9
mgladki <maciej.gladki@gmail.com>
2017-10-03 11:52:11 +0200

    test(): tests for lengthy fixing soft error

--- src/main/java/rcms/utilities/daqexpert/reasoning/logic/failures/fixingSoftErrors/LengthyFixingSoftError.java
+++ src/main/java/rcms/utilities/daqexpert/reasoning/logic/failures/fixingSoftErrors/LengthyFixingSoftError.java
@@ -8,65 +8,66 @@ import rcms.utilities.daqaggregator.data.SubSystem;
 import rcms.utilities.daqexpert.ExpertException;
 import rcms.utilities.daqexpert.ExpertExceptionCode;
 import rcms.utilities.daqexpert.Setting;
+import rcms.utilities.daqexpert.reasoning.base.action.SimpleAction;
 import rcms.utilities.daqexpert.reasoning.logic.basic.Parameterizable;
 import rcms.utilities.daqexpert.reasoning.logic.failures.KnownFailure;
 
 public class LengthyFixingSoftError extends KnownFailure implements Parameterizable {
 
-	public LengthyFixingSoftError() {
-		this.name = "Lengthy fixing-soft-error";
-	}
+    private static final String levelZeroProblematicState = "FixingSoftError";
+    private int thresholdPeriod;
+    private long timestampOfBegin;
+    public LengthyFixingSoftError() {
+        this.name = "Lengthy fixing-soft-error";
+        this.action = new SimpleAction("Call the DOC of subsystem {{SUBSYSTEM}}", "Ask the DCS shifter to check the status of subsystem {{SUBSYSTEM}}");
+    }
 
-	@Override
-	public void parametrize(Properties properties) {
+    @Override
+    public void parametrize(Properties properties) {
 
-		try {
-			this.thresholdPeriod = Integer.parseInt(
-					properties.getProperty(Setting.EXPERT_LOGIC_LENGHTYFIXINGSOFTERROR_THESHOLD_PERIOD.getKey()));
+        try {
+            this.thresholdPeriod = Integer.parseInt(
+                    properties.getProperty(Setting.EXPERT_LOGIC_LENGHTYFIXINGSOFTERROR_THESHOLD_PERIOD.getKey()));
 
-			this.description = "Level zero in FixingSoftError longer than " + (thresholdPeriod / 1000)
-					+ " sec. This is caused by subsystem(s) {{SUBSYSTEM}}";
+            this.description = "Level zero in FixingSoftError longer than " + (thresholdPeriod / 1000)
+                    + " sec. This is caused by subsystem(s) {{SUBSYSTEM}}";
 
-		} catch (NumberFormatException e) {
-			throw new ExpertException(ExpertExceptionCode.LogicModuleUpdateException, "Could not update LM "
-					+ this.getClass().getSimpleName() + ", number parsing problem: " + e.getMessage());
-		} catch (NullPointerException e) {
-			throw new ExpertException(ExpertExceptionCode.LogicModuleUpdateException,
-					"Could not update LM " + this.getClass().getSimpleName() + ", other problem: " + e.getMessage());
-		}
-	}
+        } catch (NumberFormatException e) {
+            throw new ExpertException(ExpertExceptionCode.LogicModuleUpdateException, "Could not update LM "
+                    + this.getClass().getSimpleName() + ", number parsing problem: " + e.getMessage());
+        } catch (NullPointerException e) {
+            throw new ExpertException(ExpertExceptionCode.LogicModuleUpdateException,
+                    "Could not update LM " + this.getClass().getSimpleName() + ", other problem: " + e.getMessage());
+        }
+    }
 
-	private int thresholdPeriod;
-	private long timestampOfBegin;
-	private static final String levelZeroProblematicState = "FixingSoftError";
+    @Override
+    public boolean satisfied(DAQ daq, Map<String, Boolean> results) {
 
-	@Override
-	public boolean satisfied(DAQ daq, Map<String, Boolean> results) {
+        boolean result = false;
 
-		boolean result = false;
+        String currentState = daq.getLevelZeroState();
 
-		String currentState = daq.getLevelZeroState();
+        if (levelZeroProblematicState.equalsIgnoreCase(currentState)) {
+            if (timestampOfBegin == 0) {
+                timestampOfBegin = daq.getLastUpdate();
+            } else {
 
-		if (levelZeroProblematicState.equalsIgnoreCase(currentState)) {
-			if (timestampOfBegin == 0) {
-				timestampOfBegin = daq.getLastUpdate();
-			} else {
+                if (timestampOfBegin + thresholdPeriod < daq.getLastUpdate()) {
+                    result = true;
+                    for (SubSystem subsystem : daq.getSubSystems()) {
+                        if (levelZeroProblematicState.equalsIgnoreCase(subsystem.getStatus())) {
+                            context.register("SUBSYSTEM", subsystem.getName());
+                        }
+                    }
+                }
 
-				if (timestampOfBegin + thresholdPeriod < daq.getLastUpdate()) {
-					result = true;
-					for (SubSystem subsystem : daq.getSubSystems()) {
-						if (levelZeroProblematicState.equalsIgnoreCase(subsystem.getStatus())) {
-							context.register("SUBSYSTEM", subsystem.getName());
-						}
-					}
-				}
+            }
+        } else {
+            timestampOfBegin = 0L;
+        }
 
-			}
-		} else {
-			timestampOfBegin = 0L;
-		}
-
-		return result;
-	}
+        return result;
+    }
 
 }
---
+++ src/test/java/rcms/utilities/daqexpert/reasoning/logic/failures/fixingSoftErrors/LengthyFixingSoftErrorTest.java
@@ -0,0 +1,61 @@
+package rcms.utilities.daqexpert.reasoning.logic.failures.fixingSoftErrors;
+
+import org.junit.Assert;
+import org.junit.Test;
+import rcms.utilities.daqaggregator.data.DAQ;
+import rcms.utilities.daqaggregator.data.SubSystem;
+import rcms.utilities.daqexpert.Setting;
+import rcms.utilities.daqexpert.reasoning.logic.basic.Parameterizable;
+import rcms.utilities.daqexpert.reasoning.logic.failures.KnownFailure;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import static org.junit.Assert.*;
+
+public class LengthyFixingSoftErrorTest {
+    @Test
+    public void satisfied() throws Exception {
+
+        KnownFailure lm = new LengthyFixingSoftError();
+
+        Properties props = new Properties();
+        props.setProperty(Setting.EXPERT_LOGIC_LENGHTYFIXINGSOFTERROR_THESHOLD_PERIOD.getKey(),"5000");
+        DAQ daq = new DAQ();
+        List<SubSystem> subsystems = new ArrayList<>();
+        SubSystem subsystem = new SubSystem();
+        subsystem.setName("TEST");
+        subsystems.add(subsystem);
+        subsystem.setStatus("FixingSoftError");
+        daq.setSubSystems(subsystems);
+        daq.setLevelZeroState("Other");
+        ((Parameterizable)lm).parametrize(props);
+
+        daq.setLastUpdate(1000);
+        Assert.assertFalse(lm.satisfied(daq,null));
+
+        daq.setLevelZeroState("FixingSoftError");
+
+        daq.setLastUpdate(2000);
+        Assert.assertFalse(lm.satisfied(daq,null));
+
+        daq.setLastUpdate(6000);
+        Assert.assertFalse(lm.satisfied(daq,null));
+
+        daq.setLastUpdate(7000);
+        Assert.assertFalse(lm.satisfied(daq,null));
+
+        daq.setLastUpdate(7001);
+        Assert.assertTrue(lm.satisfied(daq,null));
+
+        daq.setLastUpdate(8000);
+        Assert.assertTrue(lm.satisfied(daq,null));
+
+        daq.setLastUpdate(11000);
+        Assert.assertTrue(lm.satisfied(daq,null));
+
+        Assert.assertEquals("Level zero in FixingSoftError longer than 5 sec. This is caused by subsystem(s) TEST",lm.getDescriptionWithContext());
+    }
+
+}

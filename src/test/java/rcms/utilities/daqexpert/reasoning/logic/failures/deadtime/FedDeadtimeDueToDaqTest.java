838bd8ef7136b692843d9eb7f207e84219362b57
mgladki <maciej.gladki@gmail.com>
2017-11-14 17:29:23 +0100

    test(): add tests for deadtime from daq

--- src/test/java/rcms/utilities/daqexpert/reasoning/logic/failures/deadtime/FedDeadtimeDueToDaqTest.java
+++ src/test/java/rcms/utilities/daqexpert/reasoning/logic/failures/deadtime/FedDeadtimeDueToDaqTest.java
@@ -1,33 +1,73 @@
 package rcms.utilities.daqexpert.reasoning.logic.failures.deadtime;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import rcms.utilities.daqaggregator.data.DAQ;
 import rcms.utilities.daqaggregator.data.FED;
 import rcms.utilities.daqexpert.Setting;
 import rcms.utilities.daqexpert.reasoning.logic.basic.FEDDeadtime;
+import rcms.utilities.daqexpert.reasoning.logic.basic.StableBeams;
+import rcms.utilities.daqexpert.reasoning.logic.failures.FlowchartCaseTestBase;
+import rcms.utilities.daqexpert.reasoning.logic.failures.RateTooHigh;
 
+import java.net.URISyntaxException;
 import java.util.*;
 
 import static org.junit.Assert.*;
 
 public class FedDeadtimeDueToDaqTest {
 
+    /** module under test */
+    FedDeadtimeDueToDaq module;
+
+    /** outputs of other LMs used by LM under test */
+    Map<String, Boolean> results;
+
+
+    @Before
+    public void prepareForTest(){
+        module = new FedDeadtimeDueToDaq();
+        results = new HashMap<>();
+        results.put(FEDDeadtime.class.getSimpleName(), true);
+
+        Properties config = new Properties();
+        config.put(Setting.EXPERT_LOGIC_DEADTIME_THESHOLD_FED.getKey(), "2");
+        config.put(Setting.EXPERT_LOGIC_DEADTIME_BACKPRESSURE_FED.getKey(), "2");
+        module.parametrize(config);
+    }
+
+    @Test
+    public void test01() throws URISyntaxException {
+        assertTrue(module.satisfied(FlowchartCaseTestBase.getSnapshot("1507212900008.json.gz"), results));
+    }
+
+    @Test
+    public void test02() throws URISyntaxException {
+        assertTrue(module.satisfied(FlowchartCaseTestBase.getSnapshot("1480508609145.json.gz"), results));
+    }
+
+    @Test
+    public void test03() throws URISyntaxException {
+        assertTrue(module.satisfied(FlowchartCaseTestBase.getSnapshot("1507212240143.json.gz"), results));
+    }
+
+
     @Test
     public void satisfied() throws Exception {
 
         FedDeadtimeDueToDaq module = new FedDeadtimeDueToDaq();
         Properties p = new Properties();
-        Map<String,Boolean> r = new HashMap<>();
-        r.put(FEDDeadtime.class.getSimpleName(),true);
-        p.setProperty(Setting.EXPERT_LOGIC_DEADTIME_BACKPRESSURE_FED.getKey(),"2");
-        p.setProperty(Setting.EXPERT_LOGIC_DEADTIME_THESHOLD_FED.getKey(),"2");
+        Map<String, Boolean> r = new HashMap<>();
+        r.put(FEDDeadtime.class.getSimpleName(), true);
+        p.setProperty(Setting.EXPERT_LOGIC_DEADTIME_BACKPRESSURE_FED.getKey(), "2");
+        p.setProperty(Setting.EXPERT_LOGIC_DEADTIME_THESHOLD_FED.getKey(), "2");
         module.parametrize(p);
 
-        Assert.assertFalse(module.satisfied(mockTestObject(0,0), r));
-        Assert.assertFalse(module.satisfied(mockTestObject(2,2), r));
-        Assert.assertTrue(module.satisfied(mockTestObject(3,3), r));
-        Assert.assertFalse(module.satisfied(mockTestObject(3,1), r));
+        Assert.assertFalse(module.satisfied(mockTestObject(0, 0), r));
+        Assert.assertFalse(module.satisfied(mockTestObject(2, 2), r));
+        Assert.assertTrue(module.satisfied(mockTestObject(3, 3), r));
+        Assert.assertFalse(module.satisfied(mockTestObject(3, 1), r));
 
     }
 

e0aa3c48caaa2c094a689d68d7540087a795f271
mgladki <maciej.gladki@gmail.com>
2017-11-17 16:24:27 +0100

    fix(): update the message and suggestion for HLT Problem

--- src/main/java/rcms/utilities/daqexpert/reasoning/logic/failures/backpressure/HLTProblem.java
+++ src/main/java/rcms/utilities/daqexpert/reasoning/logic/failures/backpressure/HLTProblem.java
@@ -3,6 +3,7 @@ package rcms.utilities.daqexpert.reasoning.logic.failures.backpressure;
 import java.util.Map;
 
 import rcms.utilities.daqaggregator.data.DAQ;
+import rcms.utilities.daqexpert.reasoning.base.action.SimpleAction;
 import rcms.utilities.daqexpert.reasoning.logic.basic.NoRateWhenExpected;
 
 /**
@@ -14,10 +15,15 @@ public class HLTProblem extends BackpressureAnalyzer {
 	public HLTProblem() {
 		this.name = "HLT problem";
 
-		this.description = "All BUs are in Blocked or Cloud"
+		this.description = "There are no requests on the RUs. No BUs are in enabled state. " +
+				"Some FUs are quarantined {{FUS-QUARANTINED-PERCENTAGE}}, " +
+				"(this means that CMSSW crashed already 5 times). "
 				+ "This causes backpressure at FED {{AFFECTED-FED}} in partition {{AFFECTED-TTCP}} of {{AFFECTED-SUBSYSTEM}}";
 
-		this.action = null;
+		this.action = new SimpleAction("Call the HLT DOC and tell him/her that HLT processes crash. " +
+				"This is most likely a problem in the L1 or HLT menu. " +
+				"Another cause could be that the Frontier/DB service failed. ",
+				"Please also call the DAQ DOC, as he has to clean up the F3 farm before the next run can start.");
 
 	}
 

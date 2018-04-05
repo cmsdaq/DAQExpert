e655cd9bc4a3c46e49f75bbe888d3f9f2a3e2d43
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2018-04-05 14:54:10 +0200

    introduced a configurable parameter "threshold" to define what "zero rate" is in class NoRate. Did however NOT adapt the error message to the actual threshold.

--- src/main/java/rcms/utilities/daqexpert/reasoning/logic/basic/NoRate.java
+++ src/main/java/rcms/utilities/daqexpert/reasoning/logic/basic/NoRate.java
@@ -1,9 +1,13 @@
 package rcms.utilities.daqexpert.reasoning.logic.basic;
 
 import java.util.Map;
+import java.util.Properties;
 
 import rcms.utilities.daqaggregator.data.DAQ;
 import rcms.utilities.daqexpert.reasoning.base.ActionLogicModule;
+import rcms.utilities.daqexpert.ExpertException;
+import rcms.utilities.daqexpert.ExpertExceptionCode;
+import rcms.utilities.daqexpert.Setting;
 import rcms.utilities.daqexpert.reasoning.base.SimpleLogicModule;
 import rcms.utilities.daqexpert.reasoning.base.enums.ConditionGroup;
 import rcms.utilities.daqexpert.reasoning.base.enums.ConditionPriority;
@@ -11,8 +15,16 @@ import rcms.utilities.daqexpert.reasoning.base.enums.ConditionPriority;
 /**
  * This logic module identifies no rate condition in DAQ
  */
-public class NoRate extends SimpleLogicModule {
+public class NoRate extends SimpleLogicModule implements Parameterizable {
 
+	/** The threshold below which the trigger rate is considered as zero.
+	 *  With normal running conditions this value should be set to zero
+	 *  but are special running conditions (such as splashes) when
+	 *  even periodic calibration triggers are disabled and zero trigger
+	 *  rate is not necessarily an indication of a problem with data taking. 
+	 */
+	private float threshold;
+	
 	public NoRate() {
 		this.name = "No rate";
 		this.priority = ConditionPriority.DEFAULTT;
@@ -24,10 +36,25 @@ public class NoRate extends SimpleLogicModule {
 	 */
 	@Override
 	public boolean satisfied(DAQ daq, Map<String, Boolean> results) {
-		if (daq.getFedBuilderSummary().getRate() == 0)
+		if (daq.getFedBuilderSummary().getRate() <= threshold)
 			return true;
 		else
 			return false;
+	}
+
+	@Override
+	public void parametrize(Properties properties) {
+		try {
+			this.threshold = Integer
+					.parseInt(properties.getProperty(Setting.EXPERT_LOGIC_NO_TRIGGER_RATE_THRESHOLD.getKey()));
+
+		} catch (NumberFormatException e) {
+			throw new ExpertException(ExpertExceptionCode.LogicModuleUpdateException, "Could not update LM "
+					+ this.getClass().getSimpleName() + ", number parsing problem: " + e.getMessage());
+		} catch (NullPointerException e) {
+			throw new ExpertException(ExpertExceptionCode.LogicModuleUpdateException,
+					"Could not update LM " + this.getClass().getSimpleName() + ", other problem: " + e.getMessage());
+		}
 	}
 
 }

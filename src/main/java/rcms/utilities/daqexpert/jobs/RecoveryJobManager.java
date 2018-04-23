991845bf83922ea4f1f5c113e42c5bdb4475f66e
Maciej <maciej.gladki@gmail.com>
2018-04-23 14:37:23 +0200

    feat(): update and document recovery job manager #169

--- src/main/java/rcms/utilities/daqexpert/jobs/RecoveryJobManager.java
+++ src/main/java/rcms/utilities/daqexpert/jobs/RecoveryJobManager.java
@@ -1,146 +1,228 @@
 package rcms.utilities.daqexpert.jobs;
 
-import org.apache.commons.lang3.tuple.Pair;
 import org.apache.log4j.Logger;
 import rcms.utilities.daqexpert.persistence.Condition;
-import rcms.utilities.daqexpert.persistence.InternalConditionProducer;
-import rcms.utilities.daqexpert.reasoning.base.enums.ConditionGroup;
+import rcms.utilities.daqexpert.processing.DominatingConditionSelector;
+import rcms.utilities.daqexpert.processing.context.ContextEntry;
+import rcms.utilities.daqexpert.processing.context.ObjectContextEntry;
 
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.stream.Collectors;
 
+/**
+ * Manages the recovery job distribution to the controller given output from Logic Modules (set of conditions with
+ * recovery suggestions)
+ */
 public class RecoveryJobManager {
 
     private static final Logger logger = Logger.getLogger(RecoveryJobManager.class);
-    private final InternalConditionProducer producer;
 
-    private Pair<RecoveryRequest, Condition> currentlyRunningJob;
-    private Pair<RecoveryRequest, Condition> preemptedJob;
-
-    private RecoveryJobPerformer recoveryJobPerformer;
-    private Condition recoveryCondition;
+    /** Controller client  */
+    private ExpertControllerClient expertControllerClient;
 
 
+    /** FIFO to keep the most recent conditions - kept to avoid accessing the database when controller rejects the
+     *  recovery request. With the rejection the id of current condition being the reason to do recovery is returned.
+     *  Expert will see whether this condition should be preempted, continued or postponed depending on the .
+     */
+    private final Queue<Condition> recentRecoveryConditions = new LinkedBlockingQueue(20);
 
-    private long threshold = 15000; // dont send jobs older than 2 seconds;
 
-    public RecoveryJobManager(InternalConditionProducer producer) {
-        this.recoveryJobPerformer = new RecoveryJobPerformer();
-        this.producer = producer;
+    /**
+     * Ignore all recoveries that are older than the threshold
+     */
+    private long threshold = 20000;
+
+
+    public RecoveryJobManager(ExpertControllerClient expertControllerClient) {
+        this.expertControllerClient = expertControllerClient;
     }
 
 
     /**
-     * Run recovery job
+     * Run recovery job TODO: accept single request. reuse dominating mechanism
      */
-    public void runRecoveryJob(List<Pair<RecoveryRequest, Condition>> requests) {
+    public Long runRecoveryJob(List<RecoveryRequest> requests) {
 
-        logger.info("Recovery jobs submitted: " + requests);
+        logger.info(requests.size() + " recovery jobs submitted. Now checking the age." );
 
-        Pair<RecoveryRequest, Condition> topRequest = findMostImportant(requests);
+        Date now = new Date();
 
-        logger.info("Top recovery job selected: " + topRequest);
+        Collection<RecoveryRequest> filtered = requests.stream().filter(r -> r.getCondition().getStart().getTime() > now.getTime()-threshold).collect(Collectors.toList());
 
-        if (currentlyRunningJob == null) {
-            logger.info("No other job running - selecting this one");
+        if(requests.size() != filtered.size()){
+            logger.info(requests.size() - filtered.size() + " were filtered - older than " + threshold + " ms");
+        }
 
-            Date jobStart = topRequest.getRight().getStart();
-            if (jobStart.getTime() + threshold > (new Date()).getTime()) {
-                fireRecovery(topRequest);
+
+        if(filtered.size() == 0 ){
+            logger.info("No recent recovery found. The threshold is " + threshold + "ms old");
+            return null;
+        }
+
+
+
+        logger.info("Now choosing the dominating");
+
+        Condition dominatingCondition = findMostImportant(requests.stream().map(r->r.getCondition()).collect(Collectors.toList()) );
+
+        RecoveryRequest dominatingRequest = requests.stream().filter(r -> r.getProblemId() == dominatingCondition.getId()).findFirst().orElse(null);
+
+        logger.info("Dominating recovery job selected: " + dominatingRequest.getProblemDescription() );
+
+
+        recentRecoveryConditions.add(dominatingRequest.getCondition());
+        RecoveryResponse recoveryResponse = expertControllerClient.sendRecoveryRequest(dominatingRequest);
+
+        String status = recoveryResponse.getStatus();
+        if("rejected".equalsIgnoreCase(status)){
+            // check if this is the same now
+
+            Long rejectionConditionReasonId = recoveryResponse.getRejectedDueToConditionId();
+
+            logger.info("Controller rejected the recovery request due to condition " + rejectionConditionReasonId );
+            Optional<Condition> conditionOptional =  recentRecoveryConditions.stream().filter(c->c.getId().equals(rejectionConditionReasonId)).findFirst();
+
+
+            if(conditionOptional.isPresent()){
+
+                Condition rejectionConditionReason = conditionOptional.get();
+                String decision = handleRejection(dominatingCondition, rejectionConditionReason);
+
+                logger.info("Handling rejection with decision to: " + decision);
+
+                switch (decision){
+                    case "continue" :
+                        dominatingRequest.setSameProblem(true);
+                        break;
+                    case "interrupt":
+                        dominatingRequest.setWithInterrupt(true);
+                        break;
+                    case "postpone":
+                        dominatingRequest.setWithPostponement(true);
+                        break;
+                    case "ignore":
+                    default:
+                        logger.info("Will not try second request");
+                        return dominatingRequest.getProblemId();
+
+
+                }
+
+
+                RecoveryResponse secondRecoveryRespons = expertControllerClient.sendRecoveryRequest(dominatingRequest);
+
+                if(!"accepted".equalsIgnoreCase(secondRecoveryRespons.getStatus())){
+                    logger.warn("Second request resulted with rejection." + dominatingRequest);
+                } else {
+                    logger.info("Recovery accepted by controller with second request");
+                }
             } else {
-                logger.info("Job from the past will be ignored: " + topRequest);
+                logger.warn("Could not find condition by id " + rejectionConditionReasonId + " that was the reason to reject");
             }
+
+
         } else {
-            checkRecoveryStatus();
-
-            if (interupt(topRequest)) {
-                logger.info("Found job " + topRequest + " that will interrupt currently running one " + currentlyRunningJob);
-
-                topRequest.getLeft().setWithInterrupt(true);
-                preemptedJob = currentlyRunningJob;
-                fireRecovery(topRequest);
-                checkRecoveryStatus();
-
-            } else {
-                logger.info("Current job will NOT be interrupted");
-            }
+            logger.info("Recovery accepted by controller with first request");
         }
-
-    }
-
-    private void fireRecovery(Pair<RecoveryRequest, Condition> request) {
-        currentlyRunningJob = request;
-
-        if(preemptedJob != null){
-            recoveryCondition.setDescription("Job preempted by condition " + request.getLeft().getProblemDescription());
-            recoveryCondition.setEnd(new Date());
-            producer.updateCondition(recoveryCondition);
-            preemptedJob = null;
-        }
-
-        recoveryCondition = producer.persistCondition("Recovery", new Date(), null, ConditionGroup.LHC_BEAM);
-        Long id = recoveryJobPerformer.sendRequest(request.getLeft());
-        currentlyRunningJob.getLeft().setId(id);
-    }
-
-    public void checkRecoveryStatus() {
-
-        if (currentlyRunningJob == null) {
-            return;
-        }
-
-        String status = recoveryJobPerformer.checkStatus(currentlyRunningJob.getLeft().getId());
-
-        if ("finished".equalsIgnoreCase(status)) {
-            currentlyRunningJob = null;
-            recoveryCondition.setDescription("Successfully finished");
-            recoveryCondition.setEnd(new Date());
-            producer.updateCondition(recoveryCondition);
-            logger.info("Job finished");
-        } else if ("approved".equalsIgnoreCase(status)) {
-
-            logger.info("Updating accepted job");
-            recoveryCondition.setDescription("Accepted by operator");
-            producer.updateCondition(recoveryCondition);
-            logger.info("Job accepted");
-        } else if ("rejected".equalsIgnoreCase(status)) {
-            currentlyRunningJob = null;
-            recoveryCondition.setDescription("Rejected by operator");
-            recoveryCondition.setEnd(new Date());
-            producer.updateCondition(recoveryCondition);
-            logger.info("Job rejected");
-        } else if("timeout".equalsIgnoreCase(status)){
-            // check timeout
-            currentlyRunningJob = null;
-            recoveryCondition.setDescription("Timeout. No decision from operator in time");
-            recoveryCondition.setEnd(new Date());
-            producer.updateCondition(recoveryCondition);
-            logger.info("Job timeout");
-        }
-
+        return dominatingRequest.getProblemId();
 
 
     }
 
     /**
-     * Check weather the request should interrupt currently running one
+     * Temoporary method to choose dominating condition.
+     *
+     * TODO: use common dominating mechanism
      */
-    private boolean interupt(Pair<RecoveryRequest, Condition> request) {
-        Condition currentlyRunningCondition = currentlyRunningJob.getRight();
+    private Condition findMostImportant(List<Condition> requests) {
 
-        if (request.getRight().getLogicModule().ordinal() > currentlyRunningCondition.getLogicModule().ordinal()) {
-            return true;
-        } else {
+        Condition topRequest = DominatingConditionSelector.findDominating(requests);
+
+        return topRequest;
+    }
+
+    private boolean isSameCondition(Condition currentlyDominating, Condition currentlyRejecting){
+
+        if(currentlyDominating.getLogicModule() != currentlyRejecting.getLogicModule()){
             return false;
         }
 
+        if(currentlyDominating.getTitle() != currentlyRejecting.getTitle()){
+            return false;
+        }
+
+        if(currentlyDominating.getContext() != null) {
+
+            if(currentlyRejecting.getContext() == null){
+                return false;
+            } else{
+                // compare only objects in context
+
+                for(Map.Entry<String, ContextEntry> e: currentlyDominating.getContext().entrySet()){
+
+                    if(e.getValue() instanceof ObjectContextEntry ){
+
+                        if(!currentlyRejecting.getContext().containsKey(e.getKey())){
+                            return false;
+                        } else{
+                            if(!currentlyRejecting.getContext().get(e.getKey()).equals(e.getValue())){
+                                return false;
+                            }
+                        }
+
+                    } else {
+                        // ignore statistic entries - because they change with each snapshots
+                        // and note entries
+                    }
+
+                }
+
+            }
+
+        }
+
+        return true;
+
     }
 
-    private Pair<RecoveryRequest, Condition> findMostImportant(List<Pair<RecoveryRequest, Condition>> requests) {
+    /**
+     * Handle rejection from controller. 3 possible ways:
+     * - continue, if the conditions are the same meaning that the problem is not yet resolved. Maybe other steps are necessary
+     * - ignore, if the condition that generate currently executing recovery is more important than ones that appeared afterwards
+     * - interrupt, if more important condition appeard after currently executing recovery
+     * @param currentlyDominating condition that has appeared recently and dominates all others
+     * @param currentlyRejecting condition that is now being recovered
+     * @return
+     */
+    private String handleRejection(Condition currentlyDominating, Condition currentlyRejecting){
 
-        Pair<RecoveryRequest, Condition> interupting = requests.stream().max((c1, c2) -> Integer.compare(c1.getRight().getLogicModule().ordinal(), c2.getRight().getLogicModule().ordinal())).orElse(null);
+        boolean isSame = isSameCondition(currentlyDominating, currentlyRejecting);
+
+        if(isSame){
+            return "continue"; //
+        } else{
+
+            // use dominating mechanism
+            List<Condition> twoConditions = new ArrayList<>();
+            twoConditions.add(currentlyDominating);
+            twoConditions.add(currentlyRejecting);
+            Condition top = findMostImportant(twoConditions);
+
+            if(top.getId() == currentlyRejecting.getId()){
+                //TODO: two cases here: "postpone" and "ignore"
+                return "postpone"; // currently rejecting is the most important, postpone currently dominating condition and continue with recovery
+            } else {
+                return "interrupt"; // currenty rejecting is less important, interrupt it and start recovery of currently dominating
+            }
+
+        }
+
+    }
 
 
-        return interupting;
+    public void notifyConditionFinished(Long id) {
+        expertControllerClient.sendConditionFinishedSignal(id);
     }
 }

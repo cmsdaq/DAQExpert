47f5036c212fefaf0be3c10756f7ef26d583cdd0
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2018-05-03 14:37:43 +0200

    feat(): changed TTCHardReset to TTCResync in FC5 instructions for TRACKER which is what the tracker expert also suggested this morning and which is also currently written by him in the DAQ shifter instruction twiki page, see also #176

--- src/main/java/rcms/utilities/daqexpert/reasoning/logic/failures/FlowchartCase5.java
+++ src/main/java/rcms/utilities/daqexpert/reasoning/logic/failures/FlowchartCase5.java
@@ -43,7 +43,7 @@ public class FlowchartCase5 extends KnownFailure {
 
 		//TODO: update multistep recovery: when fixed update integration test JobManagerIt.blackboxTest1
 		/* tracker specific case only when warning state */
-		action.addContextSteps("TRACKER-WARNING", "Issue a TTCHardReset once", "Problem fixed: Make an e-log entry.",
+		action.addContextSteps("TRACKER-WARNING", "Issue a TTCResync once", "Problem fixed: Make an e-log entry.",
 				"Problem not fixed: Stop the run, red recycle TRACKER, start a new run",
 				"Problem still not fixed: Call the DOC for the TRACKER");
 

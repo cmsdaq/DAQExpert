f54d845a871b4b7e79179da85c6089dadc2d210b
Maciej <maciej.gladki@gmail.com>
2018-05-16 15:18:27 +0200

    feat(): use dominating mechanism for dashboard

--- src/main/java/rcms/utilities/daqexpert/reasoning/causality/DominatingSelector.java
+++ src/main/java/rcms/utilities/daqexpert/reasoning/causality/DominatingSelector.java
@@ -4,9 +4,7 @@ import org.apache.log4j.Logger;
 import rcms.utilities.daqexpert.persistence.Condition;
 import rcms.utilities.daqexpert.processing.Requiring;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -21,13 +19,16 @@ public class DominatingSelector {
 
     public Condition selectDominating(Set<Condition> conditions) {
 
+        // ignore conditions that has end date
+        Set<Condition> filtered = conditions.stream().filter(c->c.getEnd() ==null).collect(Collectors.toSet());
+
         if(conditions.size() > 1){
             logger.info("Requirement graph will be applied to select dominating condition");
         } else{
             return conditions.iterator().next();
         }
 
-        Set<Condition> subResult1 = getLeafsFromUsageGraph(conditions);
+        Set<Condition> subResult1 = getLeafsFromUsageGraph(filtered);
 
         if(subResult1.size() > 1){
             logger.info("Causality graph will be applied to select dominating condition");
@@ -52,8 +53,8 @@ public class DominatingSelector {
     public Set<Condition> getLeafsFromUsageGraph(Set<Condition> conditions) {
 
 
-        logger.info("Initial set of conditions (before usage graph):" );
-        conditions.stream().map(c->c.getLogicModule().getLogicModule().getName()).forEach(logger::info);
+        logger.debug("Initial set of conditions (before usage graph):" + conditions);
+        conditions.stream().map(c->c.getLogicModule().getLogicModule().getName()).forEach(logger::debug);
 
         // choose nodes that are not required by anything
         Set<Requiring> requiredByOther = new HashSet<>();
@@ -65,8 +66,8 @@ public class DominatingSelector {
         Set<Condition> filtered = conditions.stream().filter(c -> !requiredByOther.contains(c.getLogicModule().getLogicModule())).collect(Collectors.toSet());
 
 
-        logger.info("Filtered set of conditions (after usage graph):" );
-        filtered.stream().map(c->c.getLogicModule().getLogicModule().getName()).forEach(logger::info);
+        logger.debug("Filtered set of conditions (after usage graph):" );
+        filtered.stream().map(c->c.getLogicModule().getLogicModule().getName()).forEach(logger::debug);
 
         return filtered;
 
@@ -74,22 +75,45 @@ public class DominatingSelector {
 
     public Set<Condition> getLeafsFromCausality(Set<Condition> conditions){
 
-        logger.info("Initial (before causality graph):" );
-        conditions.stream().map(c->c.getLogicModule().getLogicModule().getName()).forEach(logger::info);
+        logger.debug("Initial (before causality graph):" );
+        conditions.stream().map(c->c.getLogicModule().getLogicModule().getName()).forEach(logger::debug);
 
 
-        Set<CausalityNode> causedByOther = new HashSet<>();
+        Map<Condition, Set<Condition>> forbidden = new HashMap<>();
 
+        // O(n^2) computation complexity. Reasonable taking int account that this n is in order of 1-10
         for(Condition condition: conditions){
-            causedByOther.addAll(condition.getLogicModule().getLogicModule().getCausing());
+            for(Condition condition1: conditions) {
+                /* Do not compare to self */
+                if(!condition1.equals(condition)) {
+                    if (condition.getLogicModule().getLogicModule().getAffected().contains(condition1.getLogicModule().getLogicModule())) {
+
+                        if(!forbidden.containsKey(condition1)){
+                            forbidden.put(condition1, new HashSet<>());
+                        }
+
+                        forbidden.get(condition1).add(condition);
+                    }
+                }
+
+            }
         }
 
-        Set<Condition> filtered = conditions.stream().filter(c -> causedByOther.contains(c.getLogicModule().getLogicModule())).collect(Collectors.toSet());
+        Set<Condition> result = new HashSet<>();
 
-        logger.info("Filtered (after causality graph):" );
-        filtered.stream().map(c->c.getLogicModule().getLogicModule().getName()).forEach(logger::info);
+        logger.debug("Forbidden map (after causality graph):" );
+        forbidden.entrySet().stream().map(e->e.getKey().getLogicModule().name() + " by " + e.getValue().stream().map(c->c.getLogicModule().name()).collect(Collectors.toSet())).forEach(logger::debug);
 
 
-        return filtered;
+        for(Condition condition: conditions){
+            if(!forbidden.containsKey(condition)){
+                result.add(condition);
+            }
+        }
+
+        logger.debug("Filtered (after causality graph):" );
+        result.stream().map(c->c.getLogicModule().getLogicModule().getName()).forEach(logger::debug);
+
+        return result;
     }
 }
--- src/main/java/rcms/utilities/daqexpert/websocket/ConditionDashboard.java
+++ src/main/java/rcms/utilities/daqexpert/websocket/ConditionDashboard.java
@@ -1,229 +1,184 @@
 package rcms.utilities.daqexpert.websocket;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Observable;
-import java.util.Observer;
-import java.util.Set;
-
 import org.apache.log4j.Logger;
-
 import rcms.utilities.daqexpert.persistence.Condition;
 import rcms.utilities.daqexpert.reasoning.base.ContextLogicModule;
+import rcms.utilities.daqexpert.reasoning.causality.DominatingSelector;
+
+import java.util.*;
 
 /**
+ * Condition Dashboard represents the current state of Dashboard view of DAQExpert. It holds recent conditions and
+ * notifications and marks currently dominating condition in order to facilitate if for operator.
  *
+ * <p>
  * Logical Condition dashboard
+ * <p>
  *
- * ConditionWebSocketServer.sessionHandler.addCondition(condition);
- * ConditionWebSocketServer.sessionHandler.removeCurrent();
+ * ConditionWebSocketServer.sessionHandler.addCondition(condition); ConditionWebSocketServer.sessionHandler.removeCurrent();
  * ConditionWebSocketServer.sessionHandler.updateCurrent(currentCondition);
  *
  * @author Maciej Gladki (maciej.szymon.gladki@cern.ch)
- *
  */
-public class ConditionDashboard implements Observer{
+public class ConditionDashboard implements Observer {
 
-	private final static Logger logger = Logger.getLogger(ConditionDashboard.class);
+    private final static Logger logger = Logger.getLogger(ConditionDashboard.class);
 
-	/**
-	 * Current condition
-	 */
-	private Condition dominatingCondition;
+    /** Maximum number of recent conditions displayed in the dashboard. */
+    private final int maximumNumberOfConditionsHandled;
 
-	private HashMap<Long, Condition> conditions = new LinkedHashMap<>();
+    /** Current dominating condition, choosen with the common dominating-selection mechanism */
+    private Condition dominatingCondition;
 
-	private ConditionSessionHandler sessionHander;
+    /** Map of recent conditions. Limited to @ConditionDashboard. */
+    private HashMap<Long, Condition> conditions = new LinkedHashMap<>();
 
-	private final int maximumNumberOfConditionsHandled;
+    /** Client session handler*/
+    private ConditionSessionHandler sessionHander;
 
-	public ConditionDashboard(int max) {
-		this.maximumNumberOfConditionsHandled = max;
-	}
+    private DominatingSelector dominatingSelector;
 
-	private void handleUpdate(Condition condition) {
-		if (sessionHander != null) {
-			sessionHander.handleConditionUpdate(condition);
-		}
-	}
+    public ConditionDashboard(int max) {
+        this.maximumNumberOfConditionsHandled = max;
+        this.dominatingSelector = new DominatingSelector();
+    }
 
-	public void compareWithCurrentlyDominating(Condition condition){
-		// exists some unfinished
-		// TODO: add some threshold
-		if (condition.getEnd() == null) {
+    private void handleUpdate(Condition condition) {
+        if (sessionHander != null) {
+            sessionHander.handleConditionUpdate(condition);
+        }
+    }
 
-			// no condition at the moment
-			if (dominatingCondition == null) {
-				this.dominatingCondition = condition;
-			}
+    public void compareWithCurrentlyDominating(Condition condition) {
 
-			// exists other condition at the moemnt
-			else {
+        if(dominatingCondition != null){
+            Set<Condition> conditions = new HashSet<>();
+            conditions.add(condition);
+            conditions.add(dominatingCondition);
+            Condition dominating = dominatingSelector.selectDominating(conditions);
+            this.dominatingCondition = dominating;
+        } else{
+            this.dominatingCondition = condition;
+        }
+    }
 
-				// current is more important than old
-				if (condition.getPriority().ordinal() > dominatingCondition.getPriority().ordinal()) {
-					this.dominatingCondition = condition;
-				}
+    public void update(Set<Condition> conditionsProduced) {
 
-				// current is less important than old
-				else if (condition.getPriority().ordinal() < dominatingCondition.getPriority().ordinal()) {
-					// nothing to do
-				}
+        Set<Condition> addedThisRound = new HashSet<>();
+        Condition lastDominating = dominatingCondition;
 
-				// both are equally important
-				else {
+        if (dominatingCondition != null) {
+            if (dominatingCondition.getEnd() != null) {
+                dominatingCondition = null;
+            }
+        }
 
-					// current is more useful than old
-					if (condition.getLogicModule().getUsefulness() > dominatingCondition.getLogicModule()
-							.getUsefulness()) {
-						this.dominatingCondition = condition;
-
-					}
-					// current is less useful than old
-					else if (condition.getLogicModule().getUsefulness() < dominatingCondition.getLogicModule()
-							.getUsefulness()) {
-						// nothing to do
-					}
-					// both are equally useful
-					else {
-						// newest will be displayed
-						if (condition.getStart().after(dominatingCondition.getStart())) {
-							this.dominatingCondition = condition;
-						}
-					}
-
-				}
-			}
-		}
-
-	}
-
-	public void update(Set<Condition> conditionsProduced) {
-
-		Set<Condition> addedThisRound = new HashSet<>();
-		Condition lastDominating = dominatingCondition;
-
-		if (dominatingCondition != null) {
-			if(dominatingCondition.getEnd() != null){
-				dominatingCondition = null;
-			}
-		}
-
-		for (Condition condition : conditionsProduced) {
-			if (condition.isShow() && !condition.isHoldNotifications()/*
-									 * && condition.getPriority() ==
-									 * ConditionPriority.CRITICAL
-									 */
-					&& condition.getLogicModule().getLogicModule() instanceof ContextLogicModule) {
+        for (Condition condition : conditionsProduced) {
+            if (condition.isShow() && !condition.isHoldNotifications()) {
 
 
-				compareWithCurrentlyDominating(condition);
+                compareWithCurrentlyDominating(condition);
 
-				if (!conditions.containsKey(condition.getId())) {
-					if (conditions.size() >= maximumNumberOfConditionsHandled) {
-						Condition oldest = conditions.values().iterator().next();
-						logger.trace("Observers before: " + oldest.countObservers());
-						oldest.deleteObserver(this);
-						logger.trace("Observers after: " + oldest.countObservers());
-						conditions.remove(oldest.getId());
-					}
-					conditions.put(condition.getId(), condition);
-					condition.addObserver(this);
+                if (!conditions.containsKey(condition.getId())) {
+                    if (conditions.size() >= maximumNumberOfConditionsHandled) {
+                        Condition oldest = conditions.values().iterator().next();
+                        logger.trace("Observers before: " + oldest.countObservers());
+                        oldest.deleteObserver(this);
+                        logger.trace("Observers after: " + oldest.countObservers());
+                        conditions.remove(oldest.getId());
+                    }
+                    conditions.put(condition.getId(), condition);
+                    condition.addObserver(this);
 
-					addedThisRound.add(condition);
+                    addedThisRound.add(condition);
 
-				}
-			}
-		}
+                }
+            }
+        }
 
-		for(Condition condition: conditions.values()){
-			if(condition.getEnd() == null) {
-				compareWithCurrentlyDominating(condition);
-			}
-		}
+        for (Condition condition : conditions.values()) {
+            if (condition.getEnd() == null) {
+                compareWithCurrentlyDominating(condition);
+            }
+        }
 
-		if (sessionHander != null) {
-			if (lastDominating != this.dominatingCondition) {
-				// this fires also whe dominating is null - signal to the dashboard that 'all is ok' and no problem at the moment
-				sessionHander.handleDominatingConditionChange(this.dominatingCondition);
-			}
-			if (addedThisRound.size() > 0) {
-				sessionHander.handleRecentConditionsChange(addedThisRound);
-			}
-		}
-	}
+        if (sessionHander != null) {
+            if (lastDominating != this.dominatingCondition) {
+                // this fires also whe dominating is null - signal to the dashboard that 'all is ok' and no problem at the moment
+                sessionHander.handleDominatingConditionChange(this.dominatingCondition);
+            }
+            if (addedThisRound.size() > 0) {
+                sessionHander.handleRecentConditionsChange(addedThisRound);
+            }
+        }
+    }
 
-	public Condition getCurrentCondition() {
-		return dominatingCondition;
-	}
+    public Condition getCurrentCondition() {
+        return dominatingCondition;
+    }
 
-	protected Collection<Condition> getCurrentConditions() {
-		return conditions.values();
-	}
+    protected Collection<Condition> getCurrentConditions() {
+        return conditions.values();
+    }
 
-	/**
-	 * Get condition list without dominating condition
-	 *
-	 * @return
-	 */
-	public Collection<Condition> getConditionsWithoutDominatingCondition() {
-		List<Condition> result = new ArrayList<>();
-		Iterator<Condition> i = conditions.values().iterator();
+    /**
+     * Get condition list without dominating condition
+     *
+     * @return
+     */
+    public Collection<Condition> getConditionsWithoutDominatingCondition() {
+        List<Condition> result = new ArrayList<>();
+        Iterator<Condition> i = conditions.values().iterator();
 
-		while (i.hasNext()) {
-			Condition curr = i.next();
-			if (dominatingCondition == null) {
-				result.add(curr);
-			} else if (dominatingCondition.getId() != curr.getId()) {
-				result.add(curr);
-			}
-		}
-		return result;
+        while (i.hasNext()) {
+            Condition curr = i.next();
+            if (dominatingCondition == null) {
+                result.add(curr);
+            } else if (dominatingCondition.getId() != curr.getId()) {
+                result.add(curr);
+            }
+        }
+        return result;
 
-	}
+    }
 
-	public ConditionSessionHandler getSessionHander() {
-		return sessionHander;
-	}
+    public ConditionSessionHandler getSessionHander() {
+        return sessionHander;
+    }
 
-	public void setSessionHander(ConditionSessionHandler sessionHander) {
-		this.sessionHander = sessionHander;
-	}
+    public void setSessionHander(ConditionSessionHandler sessionHander) {
+        this.sessionHander = sessionHander;
+    }
 
-	@Override
-	public String toString() {
+    @Override
+    public String toString() {
 
-		StringBuilder sb = new StringBuilder();
-		sb.append("ConditionDashboard [currentCondition="
-				+ (dominatingCondition != null ? dominatingCondition.getId() : "<none>") + ", recentConditions=[");
-		logger.trace("recent size: " + conditions.size());
-		for (Condition condition : conditions.values()) {
-			sb.append(condition.getId());
-			sb.append(",");
-		}
-		logger.trace("filtered size: " + getConditionsWithoutDominatingCondition().size());
-		sb.append("], filteredConditions=[");
-		for (Condition condition : getConditionsWithoutDominatingCondition()) {
-			sb.append(condition.getId());
-			sb.append(",");
-		}
-		sb.append("]");
-		return sb.toString();
-	}
+        StringBuilder sb = new StringBuilder();
+        sb.append("ConditionDashboard [currentCondition="
+                + (dominatingCondition != null ? dominatingCondition.getId() : "<none>") + ", recentConditions=[");
+        logger.trace("recent size: " + conditions.size());
+        for (Condition condition : conditions.values()) {
+            sb.append(condition.getId());
+            sb.append(",");
+        }
+        logger.trace("filtered size: " + getConditionsWithoutDominatingCondition().size());
+        sb.append("], filteredConditions=[");
+        for (Condition condition : getConditionsWithoutDominatingCondition()) {
+            sb.append(condition.getId());
+            sb.append(",");
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 
-	@Override
-	public void update(Observable o, Object arg) {
+    @Override
+    public void update(Observable o, Object arg) {
 
-		if (o instanceof Condition) {
-			Condition condition = (Condition) o;
-			handleUpdate(condition);
-		}
+        if (o instanceof Condition) {
+            Condition condition = (Condition) o;
+            handleUpdate(condition);
+        }
 
-	}
+    }
 
 }

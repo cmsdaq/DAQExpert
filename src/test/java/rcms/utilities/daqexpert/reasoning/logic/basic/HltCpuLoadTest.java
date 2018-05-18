065eeb131e8684044d7654f62e34bc08e2bbfa79
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2018-05-18 08:40:49 +0200

    test(): added a method testHoldOffPeriod() to class HltCpuLoadTest to test holdoff mechanism of class HltCpuLoad (#186)

--- src/test/java/rcms/utilities/daqexpert/reasoning/logic/basic/HltCpuLoadTest.java
+++ src/test/java/rcms/utilities/daqexpert/reasoning/logic/basic/HltCpuLoadTest.java
@@ -1,6 +1,8 @@
 package rcms.utilities.daqexpert.reasoning.logic.basic;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import org.junit.Test;
@@ -78,6 +80,68 @@ public class HltCpuLoadTest {
 	public void testMissingLoad() {
 		// mock the situation where the HLT CPU load could not be retrieved
 		runTest(null, false);
+	}
+
+	/** test for the holdoff mechanism at the beginning of a run */
+	@Test
+	public void testHoldOffPeriod() {
+
+		final int holdOffPeriod = 10;
+
+		HltCpuLoad module = makeInstance(holdOffPeriod);
+
+		// prepare a sequence of events and expected results
+		List<HoldOffTestData> sequence = new ArrayList<HoldOffTestData>();
+
+		//                      timestamp, runOngoing, cpuLoad, expectedResult
+		sequence.add(new HoldOffTestData(0, false, 0.5f, false));
+
+		// high HLT load outside run
+		sequence.add(new HoldOffTestData(5, false, 0.95f, true));
+
+		// run starts with reasonable CPU load
+		sequence.add(new HoldOffTestData(10, true, 0.5f, false));
+
+		// too high CPU load but within holdoff period
+		sequence.add(new HoldOffTestData(19, true, 0.95f, false));
+
+		// too high CPU load after holdoff period
+		sequence.add(new HoldOffTestData(20, true, 0.95f, true));
+
+		// normal high CPU load after holdoff period
+		sequence.add(new HoldOffTestData(30, true, 0.5f, false));
+
+		// again too high CPU load after holdoff period
+		sequence.add(new HoldOffTestData(35, true, 0.95f, true));
+
+		//-----
+
+		Map<String, Output> results = new HashMap<>();
+
+		for (HoldOffTestData data : sequence) {
+			results.put(RunOngoing.class.getSimpleName(), new Output(data.runOngoing));
+
+			// needed for assigning priorities
+			results.put(StableBeams.class.getSimpleName(), new Output(true));
+			results.put(BackpressureFromHlt.class.getSimpleName(), new Output(true));
+
+			DAQ snapshot = new DAQ();
+			snapshot.setLastUpdate(data.timestamp);
+
+			// for the moment we do not have a test case snapshot for this class
+			// so we have to put high CPU load by hand
+			HltInfo hltInfo = new HltInfo();
+			hltInfo.setCpuLoad(data.cpuLoad);
+			snapshot.setHltInfo(hltInfo);
+
+			// run module to be tested
+			boolean result = module.satisfied(snapshot, results);
+
+			assertEquals("test failed for snapshot at time " + data.timestamp,
+							data.expectedResult, result);
+
+		} // loop over test sequence
+
 	}
 
 	/** contains information about a point in time for the hold off test */

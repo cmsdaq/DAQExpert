ba18458b7ac5089201750612e1878b294a132766
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2018-05-18 08:32:49 +0200

    test(): HltCpuLoadTest.makeInstance() now takes a parameter holdOffPeriod (#186)

--- src/test/java/rcms/utilities/daqexpert/reasoning/logic/basic/HltCpuLoadTest.java
+++ src/test/java/rcms/utilities/daqexpert/reasoning/logic/basic/HltCpuLoadTest.java
@@ -16,12 +16,13 @@ import rcms.utilities.daqexpert.reasoning.logic.failures.deadtime.BackpressureFr
  */
 public class HltCpuLoadTest {
 
-	private HltCpuLoad makeInstance() {
+	private HltCpuLoad makeInstance(long holdOffPeriod) {
 		HltCpuLoad result = new HltCpuLoad();
 
 		// mock properties
 		Properties properties = new Properties();
 		properties.setProperty(Setting.EXPERT_LOGIC_HLT_CPU_LOAD_THRESHOLD.getKey(), "0.9");
+		properties.setProperty(Setting.EXPERT_LOGIC_HLT_CPU_LOAD_HOLDOFF_PERIOD.getKey(), "" + holdOffPeriod);
 
 		result.parametrize(properties);
 
@@ -36,12 +37,18 @@ public class HltCpuLoadTest {
 
 		Map<String, Output> results = new HashMap<>();
 
+		results.put(RunOngoing.class.getSimpleName(), new Output(true));
+
 		// needed for assigning priorities
 		results.put(StableBeams.class.getSimpleName(), new Output(true));
 		results.put(BackpressureFromHlt.class.getSimpleName(), new Output(true));
 
 		// ensure that the RateTooHighTest module fires
-		HltCpuLoad module = makeInstance();
+		//
+		// use a zero holdoff period so we don't
+		// run into unexpected results due to being in the holdoff period
+		// after the start of a run
+		HltCpuLoad module = makeInstance(0);
 
 		DAQ snapshot = new DAQ();
 

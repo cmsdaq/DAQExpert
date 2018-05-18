1e42fbf85b761b371f96bb8ebb215d17fd80d810
Maciej <maciej.gladki@gmail.com>
2018-05-18 11:56:47 +0200

    feat(): dominating selector update

--- src/main/java/rcms/utilities/daqexpert/reasoning/causality/DominatingSelector.java
+++ src/main/java/rcms/utilities/daqexpert/reasoning/causality/DominatingSelector.java
@@ -1,8 +1,11 @@
 package rcms.utilities.daqexpert.reasoning.causality;
 
 import org.apache.log4j.Logger;
+import rcms.utilities.daqexpert.ExpertException;
+import rcms.utilities.daqexpert.ExpertExceptionCode;
 import rcms.utilities.daqexpert.persistence.Condition;
 import rcms.utilities.daqexpert.processing.Requiring;
+import rcms.utilities.daqexpert.reasoning.base.ComparatorLogicModule;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -19,22 +22,41 @@ public class DominatingSelector {
 
     public Condition selectDominating(Set<Condition> conditions) {
 
+        if(conditions.size() == 0){
+            throw new ExpertException(ExpertExceptionCode.ExpertProblem,"Bad arguments for dominating selector, 0 conditions supplied");
+        }
+
         // ignore conditions that has end date
-        Set<Condition> filtered = conditions.stream().filter(c->c.getEnd() ==null).collect(Collectors.toSet());
-        if(filtered.size() == 1)
+        Set<Condition> filtered = conditions.stream().filter(c->c.getEnd() == null).collect(Collectors.toSet());
+        if(filtered.size() == 1) {
             return filtered.iterator().next();
+        } else if(filtered.size() == 0){
+            logger.info("No dominating has been selected (after filtering unfinished conditions nothing left)");
+            return null;
+        }
 
-        logger.info("Requirement graph will be applied to select dominating condition");
+        // ignore conditions that are comparators
+        filtered = filtered.stream().filter(c->!(c.getLogicModule().getLogicModule() instanceof ComparatorLogicModule)).collect(Collectors.toSet());
+
+        logger.debug("Requirement graph will be applied to select dominating condition");
         Set<Condition> subResult1 = getLeafsFromUsageGraph(filtered);
-        if(subResult1.size() == 1)
+        if(subResult1.size() == 1) {
             return subResult1.iterator().next();
+        } else if(subResult1.size() == 0){
+            logger.info("No dominating has been selected (after applying requirement graph nothing left)");
+            return null;
+        }
 
-        logger.info("Causality graph will be applied to select dominating condition");
+        logger.debug("Causality graph will be applied to select dominating condition");
         Set<Condition> subResult2 = getLeafsFromCausality(subResult1);
-        if(subResult2.size() == 1)
+        if(subResult2.size() == 1) {
             return subResult2.iterator().next();
+        } else if(subResult2.size() == 0){
+            logger.info("No dominating has been selected (after applying causality graph nothing left)");
+            return null;
+        }
 
-        logger.info("Start date will be applied to select dominating condition");
+        logger.debug("Start date will be applied to select dominating condition");
         Date earliest = null;
         for(Condition condition: subResult2){
             if(earliest == null) {
@@ -46,10 +68,14 @@ public class DominatingSelector {
         }
         final Date earliestFinal = earliest;
 
-        Set<Condition> subResult3 = conditions.stream().filter(c->c.getStart() == earliestFinal).collect(Collectors.toSet());
+        Set<Condition> subResult3 = conditions.stream().filter(c-> earliestFinal.equals(c.getStart())).collect(Collectors.toSet());
 
-        if(subResult3.size() == 1)
+        if(subResult3.size() == 1) {
             return subResult3.iterator().next();
+        } else if(subResult3.size() == 0){
+            logger.info("No dominating has been selected (after soring based on start date nothing left)");
+            return null;
+        }
 
         logger.warn("Could not find dominating condition "+ subResult3);
         logger.warn("Selecting the first randomly "+ subResult3.iterator().next());

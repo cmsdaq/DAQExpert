ef8e47dc415e8dd757e26edab9259d5d64cb4bfb
Maciej <maciej.gladki@gmail.com>
2018-07-18 16:20:22 +0200

    feat(): description and action for LM

--- src/main/java/rcms/utilities/daqexpert/reasoning/logic/failures/FedStuckDueToDaq.java
+++ src/main/java/rcms/utilities/daqexpert/reasoning/logic/failures/FedStuckDueToDaq.java
@@ -3,6 +3,7 @@ package rcms.utilities.daqexpert.reasoning.logic.failures;
 import rcms.utilities.daqaggregator.data.DAQ;
 import rcms.utilities.daqexpert.persistence.LogicModuleRegistry;
 import rcms.utilities.daqexpert.reasoning.base.Output;
+import rcms.utilities.daqexpert.reasoning.base.action.SimpleAction;
 import rcms.utilities.daqexpert.reasoning.logic.basic.TmpUpgradedFedProblem;
 
 import java.util.Map;
@@ -11,6 +12,13 @@ public class FedStuckDueToDaq extends KnownFailure {
 
     public FedStuckDueToDaq(){
         this.name = "FED stuck due to DAQ";
+
+        this.description = "The run is blocked by {{PROBLEM-FED}}. It receives backpressure from the DAQ. " +
+                "There is nothing wrong with this FED, the problem is in the DAQ or downstream. " +
+                "There is typically another condition active for the problem in the DAQ";
+        this.briefDescription = "FED {{PROBLEM-FED}} is stuck due to DAQ";
+        this.action = new SimpleAction("Typically another condition should describe the action. If not, call the DAQ on-call");
+
     }
 
 
@@ -35,9 +43,9 @@ public class FedStuckDueToDaq extends KnownFailure {
     @Override
     public boolean satisfied(DAQ daq, Map<String, Output> results) {
 
-        // Conditions:
+        // Conditions (with pseduo hierarcyy)
         // - backprssure > 80%
-        // - warning stuck;
+        // - stuck in warning/busy
         return false;
     }
 }

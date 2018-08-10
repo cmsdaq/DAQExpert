e998bb3747fec5cbdb038d1bd92320f44a41c70a
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2018-08-10 10:07:41 +0200

    factored out functionality common with HltOutputBandwidthExtreme from class HltOutputBandwidthTooHigh to a new (common) base class HltOutputBandwidthExceeded, logging behaviour changed slightly (moved logger to base class, i.e. this is now use in both child classes, changed name of logger to the name of the base class) (#233)

--- src/main/java/rcms/utilities/daqexpert/reasoning/logic/failures/HltOutputBandwidthExtreme.java
+++ src/main/java/rcms/utilities/daqexpert/reasoning/logic/failures/HltOutputBandwidthExtreme.java
@@ -7,20 +7,17 @@ import rcms.utilities.daqexpert.Setting;
 import rcms.utilities.daqexpert.persistence.LogicModuleRegistry;
 import rcms.utilities.daqexpert.reasoning.base.Output;
 import rcms.utilities.daqexpert.reasoning.base.action.SimpleAction;
-import rcms.utilities.daqexpert.reasoning.logic.basic.Parameterizable;
-import rcms.utilities.daqexpert.reasoning.logic.failures.deadtime.BackpressureFromHlt;
 
 import java.util.Map;
 import java.util.Properties;
 
-public class HltOutputBandwidthExtreme extends KnownFailure implements Parameterizable {
+public class HltOutputBandwidthExtreme extends HltOutputBandwidthExceeded {
 
-    private double bandwidthThresholdInGbps;
     private String additionalNote = "Note that there is also backpressure from HLT.";
 
     public HltOutputBandwidthExtreme() {
+        super(Setting.EXPERT_HLT_OUTPUT_BANDWITH_EXTREME);
         this.name = "Extreme HLT output bandwidth";
-        this.bandwidthThresholdInGbps = 0;
 
         this.briefDescription = "The HLT output bandwidth is extreme: {{BANDWIDTH}}";
         this.action = new SimpleAction( "Talk to the trigger shifter and shift leader. Have them check the pre-scale column. ",
@@ -30,45 +27,34 @@ public class HltOutputBandwidthExtreme extends KnownFailure implements Parameter
 
     @Override
     public void declareRelations(){
-        require(LogicModuleRegistry.BackpressureFromHlt);
-        declareAffected(LogicModuleRegistry.BackpressureFromHlt);
+        super.declareRelations();
         require(LogicModuleRegistry.HltOutputBandwidthTooHigh);
     }
 
     @Override
     public boolean satisfied(DAQ daq, Map<String, Output> results) {
 
-
+        // do not fire if the (lower threshold) warning does not fire
+        // (for consistency reasons)
         if (!results.get(HltOutputBandwidthTooHigh.class.getSimpleName()).getResult()) {
             return false;
         }
 
-        // assign the priority based on whether we are in stable beams or not
-        assignPriority(results);
-
-        double currentOutputBandwidthInGbps = daq.getBuSummary().getFuOutputBandwidthInMB() / 1024;
-
-        boolean result = false;
-        if (bandwidthThresholdInGbps < currentOutputBandwidthInGbps) {
-            contextHandler.registerForStatistics("BANDWIDTH", currentOutputBandwidthInGbps, "GB/s", 1);
-            result = true;
-        }
-
-        return result;
+	return super.satisfied(daq, results);
     }
 
     @Override
     public void parametrize(Properties properties) {
 
+        super.parametrize(properties);
+
+        // TODO: even if bandwidthThresholdInGbps is null we should not
+        //       get a NullPointerException here
         try {
-            this.bandwidthThresholdInGbps = Double.parseDouble(properties.getProperty(Setting.EXPERT_HLT_OUTPUT_BANDWITH_EXTREME.getKey()));
             this.description = "The HLT output bandwidth is {{BANDWIDTH}} which is above the expected maximum " + bandwidthThresholdInGbps + " GB/s. " +
                     "You should not continue running in these conditions. " +
                     "Otherwise you risk problems with the NFS mounts on the FUs which can take a long time to recover. ";
 
-        } catch (NumberFormatException e) {
-            throw new ExpertException(ExpertExceptionCode.LogicModuleUpdateException, "Could not update LM "
-                    + this.getClass().getSimpleName() + ", number parsing problem: " + e.getMessage());
         } catch (NullPointerException e) {
             throw new ExpertException(ExpertExceptionCode.LogicModuleUpdateException,
                     "Could not update LM " + this.getClass().getSimpleName() + ", other problem: " + e.getMessage());
--- src/main/java/rcms/utilities/daqexpert/reasoning/logic/failures/HltOutputBandwidthTooHigh.java
+++ src/main/java/rcms/utilities/daqexpert/reasoning/logic/failures/HltOutputBandwidthTooHigh.java
@@ -1,40 +1,19 @@
 package rcms.utilities.daqexpert.reasoning.logic.failures;
 
-import org.apache.log4j.Logger;
-import rcms.utilities.daqaggregator.data.DAQ;
 import rcms.utilities.daqexpert.ExpertException;
 import rcms.utilities.daqexpert.ExpertExceptionCode;
-import rcms.utilities.daqexpert.FailFastParameterReader;
 import rcms.utilities.daqexpert.Setting;
-import rcms.utilities.daqexpert.persistence.LogicModuleRegistry;
-import rcms.utilities.daqexpert.reasoning.base.Output;
 import rcms.utilities.daqexpert.reasoning.base.action.SimpleAction;
-import rcms.utilities.daqexpert.reasoning.logic.basic.Parameterizable;
-import rcms.utilities.daqexpert.reasoning.logic.basic.RunOngoing;
-import rcms.utilities.daqexpert.reasoning.logic.failures.helper.BeginningOfRunHoldOffLogic;
 
-import java.util.Map;
 import java.util.Properties;
 
-public class HltOutputBandwidthTooHigh extends KnownFailure implements Parameterizable {
-
-    private static final Logger logger = Logger.getLogger(HltOutputBandwidthTooHigh.class);
+public class HltOutputBandwidthTooHigh extends HltOutputBandwidthExceeded {
 
     private String additionalNote = "Note that there is also backpressure from HLT.";
 
-    /** combined holdoff logic: combines beginning of run holdoff and above
-     *  threshold holdoff
-     */
-    private BeginningOfRunHoldOffLogic holdOffLogic;
-
-    /**
-     * upper end of range for expected  rate
-     */
-    private double bandwidthThresholdInGbps;
-
     public HltOutputBandwidthTooHigh() {
+        super(Setting.EXPERT_HLT_OUTPUT_BANDWITH_TOO_HIGH);
         this.name = "Too high HLT output bandwidth";
-        this.bandwidthThresholdInGbps = 0;
 
         this.briefDescription = "The HLT output bandwidth is high: {{BANDWIDTH}}";
         this.action = new SimpleAction("Talk to the trigger shifter and shift leader. " +
@@ -42,63 +21,17 @@ public class HltOutputBandwidthTooHigh extends KnownFailure implements Parameter
     }
 
     @Override
-    public void declareRelations(){
-	require(LogicModuleRegistry.RunOngoing);
-
-        require(LogicModuleRegistry.BackpressureFromHlt);
-        declareAffected(LogicModuleRegistry.BackpressureFromHlt);
-    }
-
-    @Override
-    public boolean satisfied(DAQ daq, Map<String, Output> results) {
-
-        // assign the priority based on whether we are in stable beams or not
-        assignPriority(results);
-
-        // check if we are in a run now
-        Boolean runOngoing = results.get(RunOngoing.class.getSimpleName()).getResult();
-
-        long now = daq.getLastUpdate();
-
-        double currentOutputBandwidthInGbps = daq.getBuSummary().getFuOutputBandwidthInMB() / 1024;
-        logger.trace("Current HLT output bandwidth is: " + currentOutputBandwidthInGbps);
-
-        // update the holdoff logic
-        holdOffLogic.updateInput(runOngoing, now, (float)currentOutputBandwidthInGbps);
-
-        boolean result = false;
-
-        if (holdOffLogic.satisfied()) {
-            // only update the statistics when the condition is met
-            // (HLT bandwidth above threshold and holdoffs expired)
-            contextHandler.registerForStatistics("BANDWIDTH", currentOutputBandwidthInGbps, "GB/s", 1);
-            result =  true;
-        }
-
-        return result;
-    }
-
-    @Override
     public void parametrize(Properties properties) {
 
+        super.parametrize(properties);
+
+        // TODO: even if bandwidthThresholdInGbps is null we should not
+        //       get a NullPointerException here
         try {
-            this.bandwidthThresholdInGbps = Double.parseDouble(properties.getProperty(Setting.EXPERT_HLT_OUTPUT_BANDWITH_TOO_HIGH.getKey()));
             this.description = "The HLT output bandwidth is {{BANDWIDTH}} which is above the threshold of "
                     + bandwidthThresholdInGbps + " GB/s at which delays to Rate Monitoring and Express streams can appear. " +
                     "DQM files may get truncated resulting in lower statistics. This mode of operation may be normal for special runs if experts are monitoring.";
 
-            // an integer in milliseconds is enough to describe 24 days of
-            // holdoff period...
-            Integer runOngoingHoldOffPeriod = FailFastParameterReader.getIntegerParameter(properties, Setting.EXPERT_HLT_OUTPUT_BANDWITH_RUNONGOING_HOLDOFF_PERIOD, this.getClass());
-            Integer selfHoldOffPeriod = FailFastParameterReader.getIntegerParameter(properties, Setting.EXPERT_HLT_OUTPUT_BANDWITH_SELF_HOLDOFF_PERIOD, this.getClass());
-
-            holdOffLogic = new BeginningOfRunHoldOffLogic((float)bandwidthThresholdInGbps, runOngoingHoldOffPeriod, selfHoldOffPeriod);
-
-            logger.debug("Parametrized: " + description);
-
-        } catch (NumberFormatException e) {
-            throw new ExpertException(ExpertExceptionCode.LogicModuleUpdateException, "Could not update LM "
-                    + this.getClass().getSimpleName() + ", number parsing problem: " + e.getMessage());
         } catch (NullPointerException e) {
             throw new ExpertException(ExpertExceptionCode.LogicModuleUpdateException,
                     "Could not update LM " + this.getClass().getSimpleName() + ", other problem: " + e.getMessage());

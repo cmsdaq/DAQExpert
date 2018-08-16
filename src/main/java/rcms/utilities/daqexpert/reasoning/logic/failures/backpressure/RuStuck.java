7048900ad2cabc47f396ae4cdcc996cce104f6c9
Maciej <maciej.gladki@gmail.com>
2018-08-16 11:58:52 +0200

    fix(): fix context key, add tmp instruction

--- src/main/java/rcms/utilities/daqexpert/reasoning/logic/failures/backpressure/RuStuck.java
+++ src/main/java/rcms/utilities/daqexpert/reasoning/logic/failures/backpressure/RuStuck.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import rcms.utilities.daqaggregator.data.DAQ;
 import rcms.utilities.daqexpert.persistence.LogicModuleRegistry;
 import rcms.utilities.daqexpert.reasoning.base.Output;
+import rcms.utilities.daqexpert.reasoning.base.action.SimpleAction;
 import rcms.utilities.daqexpert.reasoning.logic.basic.NoRateWhenExpected;
 
 /**
@@ -17,10 +18,10 @@ public class RuStuck extends BackpressureAnalyzer {
 		this.name = "RU stuck";
 
 		this.description = "RU {{AFFECTED-RU}} is stuck. " + "RU has more than 0 requests - {{RU-REQUESTS}}. "
-				+ "This causes backpressure at FED {{AFFECTED-FED}} in partition {{AFFECTED-TTCP}} of {{AFFECTED-SUBSYSTEM}}";
+				+ "This causes backpressure at FED {{AFFECTED-FED}} in partition {{AFFECTED-PARTITION}} of {{AFFECTED-SUBSYSTEM}}";
 		this.briefDescription = "RU {{AFFECTED-RU}} is stuck";
 
-		this.action = null;
+		this.action = new SimpleAction("Recovery suggestion not available for this problem, contact DAQ on-call to investigate");
 
 	}
 

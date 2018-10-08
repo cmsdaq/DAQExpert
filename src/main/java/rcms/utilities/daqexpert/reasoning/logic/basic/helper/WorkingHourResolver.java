54d83537345010d5a0c9ca9306fca333e0e3f285
Maciej <maciej.gladki@gmail.com>
2018-10-08 12:48:54 +0200

    feat(): add working/extended working hours resolver

---
+++ src/main/java/rcms/utilities/daqexpert/reasoning/logic/basic/helper/WorkingHourResolver.java
@@ -0,0 +1,28 @@
+package rcms.utilities.daqexpert.reasoning.logic.basic.helper;
+
+import java.time.*;
+
+public class WorkingHourResolver {
+
+    public static Time determineTime(Long epochMilli) {
+
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(epochMilli), ZoneId.of("Europe/Zurich"));
+        LocalTime localTime = LocalTime.from(localDateTime);
+
+
+        LocalTime startExtendedWorkingHour = LocalTime.of(7,0);
+        LocalTime startWorkingHour = LocalTime.of(8,30);
+        LocalTime endWorkingHour = LocalTime.of(17,30);
+        LocalTime endExtendedWorkingHour = LocalTime.of(22,0);
+
+
+        if(localTime.isAfter(startWorkingHour) && localTime.isBefore(endWorkingHour)){
+            return Time.WORKING_HOURS;
+        } else if(localTime.isAfter(startExtendedWorkingHour) && localTime.isBefore(endExtendedWorkingHour)){
+            return Time.EXTENDED_WORKING_HOURS;
+        } else {
+            return Time.OUTSIDE_OF_EXTENDED_WORKING_HOURS;
+        }
+
+    }
+}
